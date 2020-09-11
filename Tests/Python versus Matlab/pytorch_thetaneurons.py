23e62fe970196013158f8bde9d060ab1528b59c1
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-11 18:46:03 +0200

    Pytorch does not seem to be faster

--- Tests/Python versus Matlab/pytorch_thetaneurons.py
+++ Tests/Python versus Matlab/pytorch_thetaneurons.py
@@ -4,4 +4,4 @@ from pytorch_pulse import pytorch_pulse
 def pytorch_thetaneurons(t, x, e, KdivN, a):
     # Model a network of oscillators
     I_sync = pytorch_pulse(x).sum()
-    return (1 - cos(x)) + (1 + cos(x)) * (e + a * KdivN * I_sync);
+    return (1 - cos(x)).add(1 + cos(x)).matmul(e + a * KdivN * I_sync));
