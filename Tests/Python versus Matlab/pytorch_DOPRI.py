0defda4a09e62e4d612b70cba5046e109bce3142
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-11 18:32:08 +0200

    Added last changes to the testing

--- Tests/Python versus Matlab/pytorch_DOPRI.py
+++ Tests/Python versus Matlab/pytorch_DOPRI.py
@@ -5,7 +5,18 @@ def pytorch_DOPRI(originalfunc,ta,tb,x0,h,p):
     npts = int(round((tb - ta)/h + 1))
     h = (tb - ta)/(npts-1)
     dim = x0.shape
-    xout = zeros(dim[0], npts)
+
+    cuda = torch.cuda.is_available()
+    if cuda:
+        print("cuda session enabled")
+        device = torch.device("cuda")
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    else:
+        print("cpu session enabled")
+        device = torch.device("cpu")
+        torch.set_default_tensor_type('torch.FloatTensor')
+
+    xout = zeros(dim[0], npts).to(device)
     xout[:,0] = x0
 
     tout = linspace(ta,tb,npts);
