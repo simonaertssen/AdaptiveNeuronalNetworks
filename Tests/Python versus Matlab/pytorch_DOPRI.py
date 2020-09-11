cca78fc8446dc048340712b1465aea7743708100
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-11 18:33:21 +0200

    Added last changes to the testing

--- Tests/Python versus Matlab/pytorch_DOPRI.py
+++ Tests/Python versus Matlab/pytorch_DOPRI.py
@@ -1,4 +1,4 @@
-from torch import linspace, zeros
+from torch import linspace, zeros, cuda, device
 from pytorch_DOPRIstep import pytorch_DOPRIstep
 
 def pytorch_DOPRI(originalfunc,ta,tb,x0,h,p):
@@ -6,20 +6,18 @@ def pytorch_DOPRI(originalfunc,ta,tb,x0,h,p):
     h = (tb - ta)/(npts-1)
     dim = x0.shape
 
-    cuda = torch.cuda.is_available()
+    cuda = cuda.is_available()
     if cuda:
         print("cuda session enabled")
-        device = torch.device("cuda")
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        device = device("cuda")
     else:
         print("cpu session enabled")
-        device = torch.device("cpu")
-        torch.set_default_tensor_type('torch.FloatTensor')
+        device = device("cpu")
 
     xout = zeros(dim[0], npts).to(device)
     xout[:,0] = x0
 
-    tout = linspace(ta,tb,npts);
+    tout = linspace(ta,tb,npts).to(device)
     # Make new function handle to improve speed!
     def func(t, x, e=p["e"], KN=p["K"]/p["N"], a_n=p["a_n"]):
         return originalfunc(t, x, e, KN, a_n)
