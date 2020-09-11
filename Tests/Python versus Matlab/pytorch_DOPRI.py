2758bca8d48d0b9120ed4acf89ef07d0ccc47f7f
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-11 18:35:50 +0200

    Pytorch on the GPUs does not seem to be that much faster

--- Tests/Python versus Matlab/pytorch_DOPRI.py
+++ Tests/Python versus Matlab/pytorch_DOPRI.py
@@ -1,23 +1,14 @@
-from torch import linspace, zeros, cuda, device
+from torch import linspace, zeros
 from pytorch_DOPRIstep import pytorch_DOPRIstep
 
 def pytorch_DOPRI(originalfunc,ta,tb,x0,h,p):
     npts = int(round((tb - ta)/h + 1))
     h = (tb - ta)/(npts-1)
     dim = x0.shape
-
-    cuda_available = cuda.is_available()
-    if cuda_available:
-        print("cuda session enabled")
-        currentdevice = device("cuda")
-    else:
-        print("cpu session enabled")
-        currentdevice = device("cpu")
-
-    xout = zeros(dim[0], npts).to(currentdevice)
+    xout = zeros(dim[0], npts)
     xout[:,0] = x0
 
-    tout = linspace(ta,tb,npts).to(currentdevice)
+    tout = linspace(ta,tb,npts);
     # Make new function handle to improve speed!
     def func(t, x, e=p["e"], KN=p["K"]/p["N"], a_n=p["a_n"]):
         return originalfunc(t, x, e, KN, a_n)
