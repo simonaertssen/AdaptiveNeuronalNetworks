48aaa5450d2bc2ecceb4425e9ecc32f3478d4e94
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-16 16:58:26 +0200

    working on plots

--- Full scale simulations/Fully_connected_network.m
+++ Full scale simulations/Fully_connected_network.m
@@ -13,21 +13,34 @@ F = @thetaneurons;
 tnow = 0; tend = 10;
 h = 0.0025;
 
-pars.N = 8000;
-pars.a_n = 0.6667;
+pars.N = 100;
+pars.a_n = 0.666667;
 pars.eta0 = 10.75; pars.delta = 0.5; pars.K = -9;
 seed = 1; rng(seed);
 IC = randn(pars.N, 1)*2 + 1;
 pars.e = randcauchy(seed, pars.eta0, pars.delta, pars.N);
 
-%% Running the model:
+%% Running the model: theta and QIF models
 [t, thetas] = DOPRI_threshold(F, tnow, tend, IC, h, pars);
 drawthetas = spikesNaN(thetas);
 
-% f1 = figure('Renderer', 'painters', 'DefaultLegendFontSize',10);
-% plot(t, drawthetas)
+z = orderparameter(thetas);
 
-%%
+%% Drawing: the neurons over time
+ftheta = figure('Renderer', 'painters', 'Position', [100 800 1000 200]);
+
+yyaxis left
+plot(t, drawthetas, 'Color', [0, 0, 1, 0.05]);
+ylabel('$\theta_i$','Interpreter','latex', 'FontSize', 20)
+ylim([-pi, pi])
+
+yyaxis right
+plot(t, abs(z), '-k', 'LineWidth', 2.5);
+ylabel('$\vert \bar{Z}(t) \vert$','Interpreter','latex', 'FontSize', 20)
+ylim([0, 1])
+
+
+%% Drawing: the phase space
 z = orderparameter(thetas);
 f2 = figure('Renderer', 'painters', 'DefaultLegendFontSize',10);
 plot(real(z), imag(z)) 
