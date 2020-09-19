0eafc150ff26ed00f91f1c042ed1fd5521cff272
Simon Aertssen <Simonaertssen@gmail.com>
2020-09-19 12:03:52 +0200

    Plotted fi response of theta neuron, added figures folder

--- Theta Neuron Model/ModelResponseToCurrent.m
+++ Theta Neuron Model/ModelResponseToCurrent.m
@@ -12,10 +12,10 @@ set(groot,'DefaultAxesYGrid','on')
 tnow = 0; tend = 10;
 F = @thetaneuron; h = 0.001;
 
-fexcite = figure('Position', [50 800 1000 300]);
+fexcite = figure('Renderer', 'painters', 'Position', [50 800 1000 200]);
 titlefont = 15;
 labelfont = 15;
-m = 2; n = 3;
+m = 1; n = 3;
 
 %% Excitability:
 % The theta neuron model is of type 1 excitability, meaning that the frequency 
@@ -26,7 +26,6 @@ I = @excitabilitycurrent;
 drawthetas = spikesNaN(thetas);
 
 imrow(1) = subplot(m,n,1); hold on; box on;
-removewhitspace();
 
 title("Class 1 excitability", 'FontSize', titlefont, 'FontName', 'SansSerif');
 yyaxis left
@@ -34,6 +33,10 @@ ylim([-pi - 1.5, pi + 0.3]);
 plot(t, thetas, ':k', 'LineWidth', 1)
 plot(t, drawthetas, '-', 'LineWidth', 2.5, 'color', '#0072BD');
 ylabel('$\theta_i$','Interpreter','latex', 'FontSize', labelfont);
+xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
+
+idx = [1, find(isnan(drawthetas))];
+frequencies = diff(t(idx));
 
 yyaxis right
 maxy = max(I(t));
@@ -41,7 +44,6 @@ plot(t, I(t), '-', 'LineWidth', 1, 'color', '#A2142F');
 ax = gca; ax.YAxis(1).Color = 'k'; ax.YAxis(2).Color = '#A2142F';
 set(gca,'YTick', 0:maxy:maxy, 'YLim', [-250, maxy*8]);
 
-removewhitspace();
 
 
 %% Spiking behaviour:
@@ -52,21 +54,19 @@ I = @spikecurrent;
 drawthetas = spikesNaN(thetas);
 
 imrow(2) = subplot(m,n,2); hold on; box on;
-removewhitspace();
 
 title("Spiking", 'FontSize', titlefont, 'FontName', 'SansSerif');
 yyaxis left
 ylim([-pi - 1.5, pi + 0.3]);
 plot(t, thetas, ':k', 'LineWidth', 1)
 plot(t, drawthetas, '-', 'LineWidth', 2.5, 'color', '#0072BD');
+xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
 
 yyaxis right
 maxy = max(I(t));
 plot(t, I(t), '-', 'LineWidth', 1, 'color', '#A2142F');
 ax = gca; ax.YAxis(1).Color = 'k'; ax.YAxis(2).Color = '#A2142F';
 set(gca,'YTick', 0:maxy:maxy, 'YLim', [-1, maxy*8]);
-
-removewhitspace();
 
 
 %% Bursting behaviour:
@@ -77,13 +77,13 @@ I = @burstcurrent;
 drawthetas = spikesNaN(thetas);
 
 imrow(3) = subplot(m,n,3); hold on; box on;
-removewhitspace();
 
 title("Bursting", 'FontSize', titlefont, 'FontName', 'SansSerif');
 yyaxis left
 ylim([-pi - 1.5, pi + 0.3]);
 plot(t, thetas, ':k', 'LineWidth', 1)
 plot(t, drawthetas, '-', 'LineWidth', 2.5, 'color', '#0072BD');
+xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
 
 yyaxis right
 maxy = max(I(t));
@@ -93,35 +93,9 @@ ylabel('$I$','Interpreter','latex', 'FontSize', labelfont)
 ax = gca; ax.YAxis(1).Color = 'k'; ax.YAxis(2).Color = '#A2142F';
 set(gca,'YTick', 0:maxy:maxy, 'YLim', [-200, maxy*8])
 
-removewhitspace();
 
-
-%% Rebound behaviour:
-% The neuron spikes when the input current > 0
-I = @rebound;
-
-[t, thetas] = DOPRI_singleneuron(F, tnow, tend, -pi, h, I);
-drawthetas = spikesNaN(thetas);
-
-imrow(3) = subplot(m,n,3); hold on; box on;
-removewhitspace();
-
-title("Bursting", 'FontSize', titlefont, 'FontName', 'SansSerif');
-yyaxis left
-ylim([-pi - 1.5, pi + 0.3]);
-plot(t, thetas, ':k', 'LineWidth', 1)
-plot(t, drawthetas, '-', 'LineWidth', 2.5, 'color', '#0072BD');
-
-yyaxis right
-maxy = max(I(t));
-plot(t, I(t), '-', 'LineWidth', 1, 'color', '#A2142F');
-ylim([-1, max(I(t))*5]);
-ylabel('$I$','Interpreter','latex', 'FontSize', labelfont)
-ax = gca; ax.YAxis(1).Color = 'k'; ax.YAxis(2).Color = '#A2142F';
-set(gca,'YTick', 0:maxy:maxy, 'YLim', [-200, maxy*8])
-
-removewhitspace();
-
+%% Save the figure:
+print(fexcite, '../Figures/ThetaNeuronResponseToCurrent.png', '-dpng', '-r300')
 
 %% Functions:
 function I = excitabilitycurrent(t)
@@ -145,9 +119,9 @@ function I = burstcurrent(t)
     I(t > 6 & t < 7) = burst;
 end
 
-function I = burstcurrent(t)
+function I = rebound(t)
     I = zeros(size(t));
     burst = 500;
-    I(t > 2 & t < 3) = burst;
-    I(t > 6 & t < 7) = burst;
+    I(t > 2 & t < 3) = -burst;
+    I(t > 6 & t < 7) = -burst;
 end
