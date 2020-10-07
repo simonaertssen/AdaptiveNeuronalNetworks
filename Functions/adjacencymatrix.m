c686d18b1d68ba08c18a7aa4dfbaadd0d099a2e2
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-07 18:11:06 +0200

    Adjacency matrix now working fast, lets test it

---
+++ Functions/adjacencymatrix.m
@@ -0,0 +1,57 @@
+function A = adjacencymatrix(degrees_in, degrees_out)
+    % Test whether the number of elemets is the same.
+    assert(sum(degrees_in) == sum(degrees_out))
+    
+    N = numel(degrees_in);
+    nonzeros = sum(degrees_in);
+    xidx = uint16(zeros(nonzeros, 1));
+    yidx = uint16(zeros(nonzeros, 1));
+    
+    choosefrom = uint16(2:N);
+    prob_leftout = degrees_out(1);
+    probs = degrees_out(choosefrom);
+
+    start = 1;
+    for i = 1:N
+        num = degrees_in(i);
+
+        % Sample with replacement when enough probabilities are nonzero.
+        try
+            % sampling without replacement
+            chosen = datasample(choosefrom, num, 'Replace', false, 'Weights', probs.^2);
+        catch
+            % ok, there's not enough probabilities available
+            % chosen = maxk(choosefrom, num);
+            probs = probs + 1;
+            chosen = datasample(choosefrom, num, 'Replace', false, 'Weights', probs+1);
+        end
+
+        xidx(start:(start+(num-1))) = i;
+        yidx(start:(start+(num-1))) = chosen;
+
+        [~, chosenidx] = ismember(chosen, choosefrom);
+        uniqueidx = unique(chosenidx);
+        probs(uniqueidx) = probs(uniqueidx) - 1;
+
+        if i ~= N
+            choosefrom(i) = i;
+
+            tmp = probs(i);
+            probs(i) = prob_leftout;
+            prob_leftout = tmp;
+        end
+        start = start + num;
+    end
+    
+    % Create the matrix as sparse
+    A = sparse(xidx, yidx, ones(nonzeros, 1, 'logical'));
+    assert(sum(diag(A)) == 0)
+
+    diffrows = degrees_in - full(sum(A,2))';
+    diffcols = degrees_out - full(sum(A,1));
+
+    N2 = N^2; thresh = 1.0e-9;
+    assert(sum(diffrows)/N2 < thresh)
+    assert(sum(diffcols)/N2 < thresh)
+end
+
--- Tests/SolveForAlikeSudoku.m
+++ Tests/SolveForAlikeSudoku.m
@@ -1,4 +1,5 @@
 close all; clear all; clc
+addpath('../Functions');
 
 %% Making the adjacency matrix from a degree distribution
 % From P(k) ~ k_i and P(k) ~ k_j to A_ij: a combinatorial problem...
@@ -10,74 +11,68 @@ close all; clear all; clc
 % The challenge is to get k_in(i) for each row, and then to sample from
 % k_out so that we get the right number of degrees.
 
+N = 1000;
+
 %% Test using the poisson distribution of random networks
-N = 10;
-netp = 0.3;
+netp = 0.2;
 meandegree = netp*(N - 1);
 numlinks = netp*N*(N-1)/2;
 
 degrees_in = poissrnd(meandegree,N,1)';
 degrees_out = degrees_in(randperm(N)); % because these two have to contain the same number of elements
 
-% Or better: start from a known matrix:
-Astart = randn(N);
-Astart(Astart < 0) = 0;
-Astart(Astart > 0) = 1;
-Astart = Astart - diag(diag(Astart));
-degrees_in = sum(Astart,2)';
-degrees_out = sum(Astart,1);
+assert(sum(degrees_in) == sum(degrees_out))
+
+A_fixeddegree = adjacencymatrix(degrees_in, degrees_out);
+f_fixeddegree = figure('Renderer', 'painters', 'Position', [50 800 400 400]);
+imshow(full(A_fixeddegree));
+
+%% Now using scale free networks:
+degree = 5;
+kmin = 0; kmax = 1000;
+
+P = @(x) scalefreepdf(x, N, degree, kmin, kmax);
+
+x = kmin:kmax;
+degrees = kmin + P(x);
+degrees_in = randi([kmin, kmax], 1, N);
+degrees_out = degrees_in(randperm(N));
 
 assert(sum(degrees_in) == sum(degrees_out))
-P = @(x) poisspdf(x, meandegree)*N;   
 
 %% Reconstructing A
 % Using A = spalloc(N, N, nonzeros); takes 0.142176 seconds.
 % Using A = sparse(xidx, yidx, ones(nonzeros, 1, 'logical')); takes 0.049687 seconds.
+
 clc;
 tic;
 nonzeros = sum(degrees_in);
-xidx = zeros(nonzeros, 1, 'single');
-yidx = zeros(nonzeros, 1, 'single');
-choosefrom = 2:N;
+xidx = uint16(zeros(nonzeros, 1));
+yidx = uint16(zeros(nonzeros, 1));
+choosefrom = uint16(2:N);
 prob_leftout = degrees_out(1);
 probs = degrees_out(choosefrom);
 
-deleteme = zeros(N, N);
 start = 1;
 for i = 1:N
     num = degrees_in(i);
-    xidx(start:(start+(num-1))) = i;
     
     % Sample with replacement when enough probabilities are nonzero.
-    % This can only be done with a loop, one by one.
-%     chosen = zeros(num,1);
-%     for n = 1:num
-%         choice = randsample(choosefrom,1,true,probs);
-%         probs(choosefrom==choice) = probs(choosefrom==choice) - 1;
-%         chosen(n) = choice;
-%     end
-%     chosen = datasample(choosefrom, num, 'Replace', num > nnz(probs), 'Weights', probs)
-    try 
+    try
         % sampling without replacement
-        chosen = datasample(choosefrom, num, 'Replace', false, 'Weights', probs);
+        chosen = datasample(choosefrom, num, 'Replace', false, 'Weights', probs.^2);
     catch
         % ok, there's not enough probabilities available
-        chosen = maxk(choosefrom, num);
+        % chosen = maxk(choosefrom, num);
+        chosen = datasample(choosefrom, num, 'Replace', false, 'Weights', probs+1);
     end
 
+    xidx(start:(start+(num-1))) = i;
     yidx(start:(start+(num-1))) = chosen;
-
-    [~, chosenidx] = ismember(chosen, choosefrom) 
-    uniqueidx = unique(chosenidx)
-%     freq = unique(sum(chosenidx==chosenidx'), 'stable')
-%     [freq, uniqueidx] = hist(chosenidx,unique(chosenidx))
-%     frequency = sum(chosenidx==chosenidx')
-%     [freq,uniqueidx] = groupcounts(chosenidx) 
-    probs
-    probs(uniqueidx) = probs(uniqueidx) - 1;
-    probs
-    deleteme(i*ones(num,1), choosefrom(randperm(N-1,num))) = 1;
     
+    [~, chosenidx] = ismember(chosen, choosefrom);
+    uniqueidx = unique(chosenidx);
+    probs(uniqueidx) = probs(uniqueidx) - 1;    
     
     if i ~= N
         choosefrom(i) = i;
@@ -86,32 +81,22 @@ for i = 1:N
         probs(i) = prob_leftout;
         prob_leftout = tmp;
     end
-%     probs(probs < 0) = 0
     start = start + num; 
 end
-
-% Test if we have unique values:
-test = xidx + 100*yidx;
-numel(test)
-numel(unique(test))
-
 A = sparse(xidx, yidx, ones(nonzeros, 1, 'logical'));
 assert(full(sum(diag(A))) == 0)
 whos A
 toc
 
-% Test and swap rows and columns if necessary
-diffrows = degrees_in - full(sum(A,2))'
-diffcols = degrees_out - full(sum(A,1))
-[nnzrows, irows] = max(diffrows)
-[nnzcols, icols] = max(diffcols)
+assert(sum(diag(A)) == 0)
 
-for i = 1:nnzrows
-    
-end
 
-Astart
-full(A)
+imshow(full(A));
 
-assert(sum(Astart, 'all') == sum(A, 'all'))
-%Or go through and switch columns
+diffrows = degrees_in - full(sum(A,2))';
+diffcols = degrees_out - full(sum(A,1));
+
+N2 = N^2; thresh = 1.0e-9;
+assert(sum(diffrows)/N2 < thresh)
+assert(sum(diffcols)/N2 < thresh)
+
