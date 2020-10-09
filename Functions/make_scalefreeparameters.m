a4ccae2a3d6721baf1ae19a2b269e026adcf74cf
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-09 14:16:13 +0200

    tried using adjacency matrix as an attribute

--- Functions/make_scalefreeparameters.m
+++ Functions/make_scalefreeparameters.m
@@ -13,10 +13,10 @@ function scalefreepars = make_scalefreeparameters(pars, degree, kmin, kmax)
     scalefreepars.kmin = kmin;
     scalefreepars.kmax = kmax;
     scalefreepars.degree = degree;
+    
     scalefreepars.P = @(x) scalefreepdf(x, pars.N, scalefreepars.degree, kmin, kmax);
-
     scalefreepars.degrees_in = randsample(kmin:kmax, pars.N, true, scalefreepars.P(kmin:kmax));
-
+    
     fsolveoptions = optimset('Display','off');
     scalefreepars.meandegree = fsolve(@(z) scalefreepars.P(z) - mean(scalefreepars.P(kmin:kmax)), kmin, fsolveoptions);
 end
