6ccd851010ed4a9cb12cf78d07c37bf8c92b5ce8
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-09 19:41:46 +0200

    Faster implementation

--- Functions/thetaneurons.m
+++ Functions/thetaneurons.m
@@ -1,6 +1,5 @@
 function dxdt = thetaneurons(t, x, e, KdivN, a)
     p = pulse(x);
-    I_sync = sum(p) - p;
-    dxdt = (1 - cos(x)) + (1 + cos(x)).*(e + a * KdivN * I_sync);
+    dxdt = (1 - cos(x)) + (1 + cos(x)).*(e + a * KdivN * (sum(p) - p));
 end
 
