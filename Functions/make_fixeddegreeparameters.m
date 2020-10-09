51b7305715ea6f63df712a2c2d9112dd104a97d4
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-09 14:44:45 +0200

    made extra precautions

--- Functions/make_fixeddegreeparameters.m
+++ Functions/make_fixeddegreeparameters.m
@@ -1,7 +1,12 @@
 function fixeddegreepars = make_fixeddegreeparameters(pars, netdegree)
     % Make the necessery parameters for the fixeddegree / fixed degree networks
     fixeddegreepars = pars;
-    fixeddegreepars.netdegree = netdegree;
+    
+    if netdegree > pars.N - 1
+        error('Net degree too large')
+    else 
+        fixeddegreepars.netdegree = netdegree;
+    end
     fixeddegreepars.degrees_in = zeros(pars.N,1);
     fixeddegreepars.degrees_in(randperm(pars.N)) = fixeddegreepars.netdegree;
 
