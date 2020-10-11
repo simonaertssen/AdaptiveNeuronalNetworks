4f79fe950357fbfb2f3399a0448f7c786c90d9e4
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-11 19:28:43 +0200

    saving intermediate results and heavier simulations

--- Full scale simulations/Investigate_order_parameter.m
+++ Full scale simulations/Investigate_order_parameter.m
@@ -12,7 +12,7 @@ set(groot,'DefaultAxesXGrid','on')
 set(groot,'DefaultAxesYGrid','on')
 
 titlefont = 15;
-labelfont = 18;
+labelfont = 15;
 
 %% Theta model parameters:
 tnow = 0; tend = 3;
@@ -29,44 +29,61 @@ pars.e = randcauchy(seed, pars.eta0, pars.delta, pars.N);
 
 % Network distributions and parameters:
 fixeddegreepars = make_fixeddegreeparameters(pars, round(pars.N*0.3));
-A_fixeddegree = ones(pars.N) - eye(pars.N);
 randompars = make_randomparameters(pars, 0.3);
 scalefreepars = make_scalefreeparameters(pars, 3);
 
+f_PRS = figure('Renderer', 'painters', 'Position', [50 800 800 600]);
+options = odeset('RelTol', 1.0e-6,'AbsTol', 1.0e-6);
+
 for i = 1:3
+    if i == 1
+        params = fixeddegreepars;
+    elseif i == 2
+        params = randompars;
+    elseif i == 3
+        params = scalefreepars;
+    end
+    
+    % The full scale simulation using the adjacency matrix:
+    [t, thetas, A] = DOPRI_simulatenetwork(tnow,tend,IC,h,params);
+    z = orderparameter(thetas);
+    
+    % The OA mean field theory:
+    oa_params = prepareOAparameters(params);
+    OAIC = ones(oa_params.l,1)*z(1) + 0.001*randn(oa_params.l,1);
+    [Toa, b_i] = ode45(@(t,x) MFROA(t,x,oa_params), [tnow, tend], OAIC, options);
+    Z_oa = orderparameter_oa(b_i, oa_params.P, oa_params.k, oa_params.N);
+    
+    % Other order parameters:
+    degrees = sum(A,2);
+    z_net = orderparameter_net(thetas, degrees, A);
+    z_mf = orderparameter_mf(thetas, degrees);
+    z_link = orderparameter_link(thetas, degrees, A);
+
+    % Plotting
+    subplot(3,1,i); hold on; box on;
+    
+    plot(t, abs(z), 'LineWidth', 2);
+    plot(Toa, abs(Z_oa), 'LineWidth', 2);
+    plot(t, abs(z_net), 'LineWidth', 2);
+    plot(t, abs(z_mf), 'LineWidth', 2);
+    plot(t, abs(z_link), 'LineWidth', 2);
+
+    xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
+    ylabel('$\| Z (t) \|$','Interpreter','latex', 'FontSize', labelfont)
+
+    legend('Kuramoto order parameter', 'OA order parameter', 'Network order parameter', 'Mean field order parameter', 'Link field order parameter', 'FontSize', labelfont-5, 'Location', 'southeast')
+    removewhitspace();
 end
 
 
-% The full scale simulation using the adjacency matrix:
-fixeddegreepars = make_fixeddegreeparameters(pars, pars.N - 1);
-[t, thetas] = DOPRI_simulatenetwork(tnow,tend,IC,h,fixeddegreepars);
-z = orderparameter(thetas);
 
-% The OA mean field theory:
-fixeddegreepars = prepareOAparameters(fixeddegreepars);
-OAIC = ones(fixeddegreepars.l,1)*z(1) + 0.001*randn(fixeddegreepars.l,1);
-[Toa, b_i] = ode45(@(t,x) MFROA(t,x,fixeddegreepars), [tnow, tend], OAIC, options);
-Zoa = b_i * fixeddegreepars.P(fixeddegreepars.k)/fixeddegreepars.N;
 
-degrees = sum(A_fixeddegree,2);
-z_net = orderparameter_net(thetas, degrees, A_fixeddegree);
-z_mf = orderparameter_mf(thetas, degrees);
-z_link = orderparameter_link(thetas, degrees, A_fixeddegree);
+
 
 
 %%
-f_fixeddegree = figure('Renderer', 'painters', 'Position', [50 800 800 200]);
 
-hold on;
-plot(t, abs(z), 'LineWidth', 2);
-plot(t, abs(z_net), 'LineWidth', 2);
-plot(t, abs(z_mf), 'LineWidth', 2);
-plot(t, abs(z_link), 'LineWidth', 2);
 
-xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
-ylabel('$\| Z (t) \|$','Interpreter','latex', 'FontSize', labelfont)
-
-legend('Kuramoto order parameter', 'Network order parameter', 'Mean field order parameter', 'Link field order parameter', 'FontSize', labelfont-5, 'Location', 'southeast')
-removewhitspace();
 
 
