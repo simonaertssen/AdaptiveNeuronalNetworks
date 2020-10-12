bb512e5a926d861adcf67a00b95915757b42dcc0
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-12 09:00:35 +0200

    Getting better plots

--- Full scale simulations/Investigate_order_parameter.m
+++ Full scale simulations/Investigate_order_parameter.m
@@ -15,8 +15,8 @@ titlefont = 15;
 labelfont = 15;
 
 %% Theta model parameters:
-tnow = 0; tend = 3;
-h = 0.01;
+tnow = 0; tend = 10;
+h = 0.001;
 
 pars.N = 100;
 pars.a_n = 0.666667;
@@ -35,9 +35,6 @@ scalefreepars = make_scalefreeparameters(pars, 3);
 f_PRS = figure('Renderer', 'painters', 'Position', [50 800 800 600]);
 options = odeset('RelTol', 1.0e-6,'AbsTol', 1.0e-6);
 
-titles = [sprintf('\\bf Fixed-degree network: $$\\langle k \\rangle$$ = %0.1f', fixeddegreepars.meandegree), ...
-          sprintf('\\bf Random network: $$\\langle k \\rangle$$ = %0.1f', randompars.meandegree), ...
-          sprintf('\\bf Scale-free network: $$\\langle k \\rangle$$ = %0.1f', scalefreepars.meandegree)];
 for i = 1:3
     if i == 1
         params = fixeddegreepars;
@@ -64,7 +61,7 @@ for i = 1:3
     z_link = orderparameter_link(thetas, degrees, A);
 
     % Plotting
-    subplot(3,1,i); hold on; box on;
+    imrow(i) = subplot(3,1,i); hold on; box on;
     
     plot(t, abs(z), 'LineWidth', 2);
     plot(Toa, abs(Z_oa), 'LineWidth', 2);
@@ -73,10 +70,71 @@ for i = 1:3
     plot(t, abs(z_link), 'LineWidth', 2);
 
     ylabel('$\| Z (t) \|$','Interpreter','latex', 'FontSize', labelfont)
-    title(titles(i), 'FontSize', titlefont, 'Interpreter', 'latex');
 
     removewhitspace();
 end
+
+set(imrow(1).Title,'String', sprintf('\\bf Fixed-degree network:  $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, fixeddegreepars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex');
+set(imrow(2).Title,'String', sprintf('\\bf Random network:  $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, randompars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex');
+set(imrow(3).Title,'String', sprintf('\\bf Scale-free network:  $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, scalefreepars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex');
+
+legend('Kuramoto order parameter', 'OA order parameter', 'Network order parameter', 'Mean field order parameter', 'Link field order parameter', 'FontSize', labelfont-5, 'Location', 'southeast')
+xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
+
+%% PSS state: one single stable focus
+pars.eta0 = 0.5; pars.delta = 0.7; pars.K = 2;
+pars.e = randcauchy(seed, pars.eta0, pars.delta, pars.N);
+
+% Network distributions and parameters:
+fixeddegreepars = make_fixeddegreeparameters(pars, round(pars.N*0.3));
+randompars = make_randomparameters(pars, 0.3);
+scalefreepars = make_scalefreeparameters(pars, 3);
+
+f_PRS = figure('Renderer', 'painters', 'Position', [50 800 800 600]);
+options = odeset('RelTol', 1.0e-6,'AbsTol', 1.0e-6);
+
+for i = 1:3
+    if i == 1
+        params = fixeddegreepars;
+    elseif i == 2
+        params = randompars;
+    elseif i == 3
+        params = scalefreepars;
+    end
+    
+    % The full scale simulation using the adjacency matrix:
+    [t, thetas, A] = DOPRI_simulatenetwork(tnow,tend,IC,h,params);
+    z = orderparameter(thetas);
+    
+    % The OA mean field theory:
+    oa_params = prepareOAparameters(params);
+    OAIC = ones(oa_params.l,1)*z(1) + 0.001*randn(oa_params.l,1);
+    [Toa, b_i] = ode45(@(t,x) MFROA(t,x,oa_params), [tnow, tend], OAIC, options);
+    Z_oa = orderparameter_oa(b_i, oa_params.P, oa_params.k, oa_params.N);
+    
+    % Other order parameters:
+    degrees = sum(A,2);
+    z_net = orderparameter_net(thetas, degrees, A);
+    z_mf = orderparameter_mf(thetas, degrees);
+    z_link = orderparameter_link(thetas, degrees, A);
+
+    % Plotting
+    imrow(i) = subplot(3,1,i); hold on; box on;
+    
+    plot(t, abs(z), 'LineWidth', 2);
+    plot(Toa, abs(Z_oa), 'LineWidth', 2);
+    plot(t, abs(z_net), 'LineWidth', 2);
+    plot(t, abs(z_mf), 'LineWidth', 2);
+    plot(t, abs(z_link), 'LineWidth', 2);
+
+    ylabel('$\| Z (t) \|$','Interpreter','latex', 'FontSize', labelfont)
+
+    removewhitspace();
+end
+
+set(imrow(1).Title,'String', sprintf('\\bf Fixed-degree network:  $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, fixeddegreepars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex');
+set(imrow(2).Title,'String', sprintf('\\bf Random network:  $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, randompars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex');
+set(imrow(3).Title,'String', sprintf('\\bf Scale-free network:  $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, scalefreepars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex');
 
 legend('Kuramoto order parameter', 'OA order parameter', 'Network order parameter', 'Mean field order parameter', 'Link field order parameter', 'FontSize', labelfont-5, 'Location', 'southeast')
 xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
