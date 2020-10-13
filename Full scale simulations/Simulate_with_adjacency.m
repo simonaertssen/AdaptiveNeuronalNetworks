ee74f6b9bf6fb24bc399c4c54afc12529b64c950
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-13 10:48:51 +0200

    gotta get the GPU working

--- Full scale simulations/Simulate_with_adjacency.m
+++ Full scale simulations/Simulate_with_adjacency.m
@@ -23,7 +23,7 @@ initarray = make_GPUhandle();
 tnow = 0; tend = 10;
 h = 0.001;
 
-pars.N = 10000;
+pars.N = 1000;
 pars.a_n = 0.666667;
 pars.eta0 = 10.75; pars.delta = 0.5; pars.K = -9;
 % pars.eta0 = -0.9; pars.delta = 0.8; pars.K = -2;
--- Functions/thetaneurons_full.m
+++ Functions/thetaneurons_full.m
@@ -1,4 +1,5 @@
 function dxdt = thetaneurons_full(t, x, K, A, e, meankinverted, a)  
     dxdt = (1 - cos(x)) + (1 + cos(x)).*(e + a * K * meankinverted * (A * pulse(x)));
+    dxdt = gather(dxdt);
 end
 
