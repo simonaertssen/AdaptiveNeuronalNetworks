519467a718771b42797b2f834929de80fddee97d
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-14 15:57:39 +0200

    probably found the error

--- Full scale simulations/ValidateCPWcycle.m
+++ Full scale simulations/ValidateCPWcycle.m
@@ -14,8 +14,9 @@ labelfont = 15;
 
 %% Theta model parameters:
 tnow = 0; tend = 5;
+h = 0.001;
 
-pars.N = 1000;
+pars.N = 3;
 pars.a_n = 0.666667;
 pars.eta0 = 10.75; pars.delta = 0.5; pars.K = -9;
 
@@ -33,22 +34,26 @@ initarray = make_GPUhandle();
 
 %% 0. Perform a full scale simulation of a FULLY CONNECTED network:
 tic;
-[t, thetas] = DOPRI_threshold(@thetaneurons, tnow, tend, IC, 0.001, pars);
+[t, thetas] = DOPRI_threshold(@thetaneurons, tnow, tend, IC, h, pars);
 z = orderparameter(thetas);
 toc
 
 tic;
-[tode45, theta_ode45] = ode45(@(t,x) thetaneurons(t,x,pars.e,pars.K/pars.N,pars.a_n), [tnow, tend], IC, odeoptions);
+[t_ode45, theta_ode45] = ode45(@(t,x) thetaneurons(t,x,pars.e,pars.K/pars.N,pars.a_n), [tnow, tend], IC, odeoptions);
 theta_ode45 = wrapToPi(theta_ode45)';
 zode45 = orderparameter(theta_ode45)';
 toc
 
-tic
 fdpars = make_fixeddegreeparameters(pars, pars.N - 1);
-A = initarray(adjacencymatrix(fdpars.degrees_in, fdpars.degrees_out));
-[t_full, thetas_full] = ode45(@(t,x,K) thetaneurons_full(t,x,fdpars.K,A,fdpars.e,1/fdpars.meandegree,fdpars.a_n), [tnow, tend], IC, odeoptions);
-thetas_full = wrapToPi(thetas_full)';
-z_full = orderparameter(thetas_full)';
+tic;
+[t_full, thetas_full, A] = DOPRI_simulatenetwork(tnow, tend, IC, h, fdpars, fdpars.K);
+z_full = orderparameter(thetas_full);
+toc
+
+tic
+[t_fullode45, thetas_fullode45] = ode45(@(t,x,K) thetaneurons_full(t,x,fdpars.K,A,fdpars.e,1/fdpars.meandegree,fdpars.a_n), [tnow, tend], IC, odeoptions);
+thetas_fullode45 = wrapToPi(thetas_fullode45)';
+z_fullode45 = orderparameter(thetas_fullode45)';
 toc
 
 %% The mean field theory for fixed degree networks:
@@ -60,13 +65,14 @@ f_CPW = figure('Renderer', 'painters', 'Position', [50 800 800 400]); box on; ho
 
 xlim([tnow, tend]); ylim([0, 1])
 plot(t, abs(z), '-', 'LineWidth', 2);
-plot(tode45, abs(zode45), '-', 'LineWidth', 2);
+plot(t_ode45, abs(zode45), '-', 'LineWidth', 2);
 plot(t_full, abs(z_full), '-', 'LineWidth', 2);
+plot(t_fullode45, abs(z_fullode45), '-', 'LineWidth', 2);
 plot(T, abs(Z), '-', 'LineWidth', 2);
 xlabel('$$t$$', 'Interpreter', 'latex', 'FontSize', labelfont);
 ylabel('$\vert Z (t) \vert$','Interpreter','latex', 'FontSize', labelfont)
 
-legend('$$Z(t)_{\rm simple}$$', '$$Z(t)_{A_{ij}}$$', '$$Z(t)_{\rm ode45}$$', '$$\overline{Z(t)}_{\rm MF}$$', 'Interpreter', 'latex', 'FontSize', labelfont, 'Location', 'southwest')
+legend('$$Z(t)_{\rm simple}$$', '$$Z(t)_{\rm ode45}$$', '$$Z(t)_{A_{ij}}$$', '$$Z(t)_{A_{ij}, \rm ode45}$$', '$$\overline{Z(t)}_{\rm MF}$$', 'Interpreter', 'latex', 'FontSize', labelfont, 'Location', 'southwest')
 title(sprintf('\\bf Fully connected network: $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f', pars.N, fdpars.meandegree), 'FontSize', titlefont, 'Interpreter', 'latex')
 removewhitspace();
 
