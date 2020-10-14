a628bde0cf894e25bbb7c1d0bbccbdedd0e61bcf
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-14 16:07:58 +0200

    back to vector representation

--- Mean Field Reductions/MFR2.m
+++ Mean Field Reductions/MFR2.m
@@ -1,10 +1,10 @@
 function dzdt = MFR2(t, z, p)
 % Here we compute the differential equation for the mean field reduction 
 % using the Ott-Antonsen reduction (see Luke 2013).    
-    one = -1i*(z-1)*(z-1)*0.5;
-    two = (z+1)*(z+1);
+    one = -1i*(z-1).*(z-1)*0.5;
+    two = (z+1).*(z+1);
     zc = conj(z);
-    H = (1 + (z*z + zc*zc)/6 - 4*real(z)/3);
-    dzdt = one + 0.5*two*(-p.delta + 1i*p.eta0 + 1i*p.K*H);
+    H = (1 + (z.*z + zc.*zc)/6 - 4*real(z)/3);
+    dzdt = one + 0.5*two.*(-p.delta + 1i*p.eta0 + 1i*p.K*H);
 end
 
