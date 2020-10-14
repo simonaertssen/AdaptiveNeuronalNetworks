c644b11d070a897299261b1b0857d56c19033bf7
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-14 16:13:08 +0200

    Correct, and added gather

--- Functions/thetaneurons.m
+++ Functions/thetaneurons.m
@@ -1,4 +1,4 @@
 function dxdt = thetaneurons(t, x, e, KdivN, a)
-    dxdt = (1 - cos(x)) + (1 + cos(x)).*(e + a * KdivN * sum(pulse(x)));
+    dxdt = gather((1 - cos(x)) + (1 + cos(x)).*(e + a * KdivN * sum(pulse(x))));
 end
 
--- Functions/thetaneurons_full.m
+++ Functions/thetaneurons_full.m
@@ -1,5 +1,4 @@
 function dxdt = thetaneurons_full(t, x, K, A, e, meankinverted, a)  
-    dxdt = (1 - cos(x)) + (1 + cos(x)).*(e + a * K * meankinverted * (A * pulse(x)));
-    dxdt = gather(dxdt);
+    dxdt = gather((1 - cos(x)) + (1 + cos(x)).*(e + a * K * meankinverted * (A * pulse(x))));
 end
 
