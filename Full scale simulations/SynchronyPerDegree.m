38ce548fd19425fe876fb2c3bc014acaee821976
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-19 18:20:56 +0200

    starting to get ready for the synchrony measurements

--- Full scale simulations/SynchronyPerDegree.m
+++ Full scale simulations/SynchronyPerDegree.m
@@ -24,7 +24,7 @@ initarray = make_GPUhandle();
 tnow = 0; tend = 1;
 h = 0.001;
 
-pars.N = 20;
+pars.N = 100;
 pars.a_n = 0.666666666666666666667;
 pars.eta0 = 10.75; pars.delta = 0.5; pars.K = -9;
 pars.eta0 = -0.9; pars.delta = 0.8; pars.K = -2;
@@ -80,12 +80,10 @@ zfull_hi = orderparameter(thetasfull(highidx,:));
 f_fixeddegree = figure('Renderer', 'painters', 'Position', [50 800 800 400]); box on; hold on;
 
 xlim([tnow, tend]); ylim([0, 1])
-% plot(tfull, abs(zfull), '-', 'LineWidth', 4, 'Color', '#0072BD');
-
+plot(tfull, abs(zfull), '-', 'LineWidth', 4, 'Color', '#0072BD');
 plot(tfull, abs(zfull_lo), '-', 'LineWidth', 2, 'Color', '#D95319');
 plot(tfull, abs(zfull_hi), '-', 'LineWidth', 2, 'Color', '#EDB120');
-
-% plot(TOA, abs(ZOA), '-', 'LineWidth', 3, 'Color', '#000000');
+plot(TOA, abs(ZOA), '-', 'LineWidth', 3, 'Color', '#000000');
 
 xlabel('$$t$$', 'Interpreter', 'latex', 'FontSize', labelfont);
 ylabel('$\vert Z (t) \vert$','Interpreter','latex', 'FontSize', labelfont)
@@ -117,11 +115,11 @@ zfull_hi = orderparameter(thetasfull(highidx));
 
 % Order parameter per degree:
 nbins = 4;
-[N,edges] = histcounts(rdpars.degrees_in, nbins)
-zdegrees = zeros(numel(tfull), nbins);  
+[N,edges] = histcounts(rdpars.degrees_in, nbins);
+zdegrees = zeros(nbins, numel(tfull));  
 for i = 1:nbins
-    idx = rdpars.degrees_in > edges(i) & rdpars.degrees_in < edges(i+1)
-    zdegrees(:,i) = thetasfull(idx,:);
+    idx = rdpars.degrees_in > edges(i) & rdpars.degrees_in < edges(i+1);
+    zdegrees(i,:) = orderparameter(thetasfull(idx,:));
 end
 
 %% Plotting the results:
@@ -129,18 +127,21 @@ f_random = figure('Renderer', 'painters', 'Position', [50 800 800 400]); box on;
 
 xlim([tnow, tend]); ylim([0, 1])
 plot(tfull, abs(zfull), '-', 'LineWidth', 3, 'Color', '#0072BD');
-plot(TOA, abs(ZOA), '-', 'LineWidth', 2, 'Color', '#000000');
-
 plot(tfull, abs(zfull_lo), '-', 'LineWidth', 2, 'Color', '#D95319');
 plot(tfull, abs(zfull_hi), '-', 'LineWidth', 2, 'Color', '#EDB120');
+plot(TOA, abs(ZOA), '-', 'LineWidth', 2, 'Color', '#000000');
+
+for i = 1:nbins
+    plot(tfull, zdegrees(i,:), '-', 'LineWidth', 2, 'Color', [0, 1/nbins*i, 0])
+end
 
 xlabel('$$t$$', 'Interpreter', 'latex', 'FontSize', labelfont);
 ylabel('$\vert Z (t) \vert$','Interpreter','latex', 'FontSize', labelfont)
 
 title(sprintf('\\bf Random network: $$N$$ = %d, $$\\langle k \\rangle$$ = %0.1f, $$p$$ = %0.1f', pars.N, rdpars.meandegree, rdpars.netp), 'FontSize', titlefont, 'Interpreter', 'latex')
-legend('$$Z(t)_{A_{ij}}$$', '$$\overline{Z(t)}_{MF_{OA}}$$', 'Interpreter', 'latex', 'FontSize', labelfont, 'Location', 'northwest', 'Orientation','horizontal')
+legend('$$Z(t)_{A_{ij}}$$', '$$Z(t)_{\eta_{\rm  < 0.05}}$$', '$$Z(t)_{\eta_{\rm > 0.95}}$$', '$$\overline{Z(t)}_{MF_{OA}}$$', 'Interpreter', 'latex', 'FontSize', labelfont, 'Location', 'southwest', 'Orientation','horizontal')
 exportpdf(f_random, '../Figures/SynchronyRandom.pdf', export);
-close(f_random)
+% close(f_random)
 
 disp('Made random network figure')
 
