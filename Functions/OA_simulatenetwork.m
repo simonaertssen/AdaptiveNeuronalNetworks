1a2cfdfd3b26ee372f29ce8c3ef40e2af19d5bc6
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-20 14:17:55 +0200

    do start from same ICs

--- Functions/OA_simulatenetwork.m
+++ Functions/OA_simulatenetwork.m
@@ -14,7 +14,10 @@ function [TOA, ZOA, b] = OA_simulatenetwork(tnow, tend, IC, p, odeoptions)
 %         error('IC might be wrong?')
 %     end
 %     
-    OAIC = IC*ones(1,p.l);
+    OAIC = zeros(1,p.l);
+    for i = 1:p.l
+        OAIC(i) = sum(exp(1i*IC(p.degrees_in == p.k(i)))) / p.P(p.k(i));
+    end
 
     [TOA, b] = ode45(@(t,x) MFROA(t,x,p), [tnow, tend], gather(OAIC), odeoptions);
     ZOA = b*p.P(p.k)/p.N;
