a5ba5ef5759747b17c4793e13e47707d9ef67164
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-20 13:44:46 +0200

    Get the smae ic different times

--- Functions/OA_simulatenetwork.m
+++ Functions/OA_simulatenetwork.m
@@ -3,16 +3,18 @@ function [TOA, ZOA, b] = OA_simulatenetwork(tnow, tend, IC, p, odeoptions)
         odeoptions = odeset('RelTol', 1.0e-6,'AbsTol', 1.0e-6);
     end
     
-    if isvector(IC)
-        OAIC = zeros(1,p.l);
-        for i = 1:p.l
-            OAIC(i) = sum(exp(1i*IC(p.degrees_in == p.k(i)))) / p.P(p.k(i));
-        end
-    elseif isscalar(IC)
-        OAIC = IC*ones(1,p.l);
-    else
-        error('IC might be wrong?')
-    end
+%     if isvector(IC)
+%         OAIC = zeros(1,p.l);
+%         for i = 1:p.l
+%             OAIC(i) = sum(exp(1i*IC(p.degrees_in == p.k(i)))) / p.P(p.k(i));
+%         end
+%     elseif isscalar(IC)
+%         OAIC = IC*ones(1,p.l);
+%     else
+%         error('IC might be wrong?')
+%     end
+%     
+    OAIC = IC*ones(1,p.l);
 
     [TOA, b] = ode45(@(t,x) MFROA(t,x,p), [tnow, tend], gather(OAIC), odeoptions);
     ZOA = b*p.P(p.k)/p.N;
