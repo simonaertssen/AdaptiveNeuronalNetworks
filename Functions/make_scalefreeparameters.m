e14d7d913ce32231773393d36c3cb5b49e9cd746
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-22 10:43:32 +0200

    The number of links  should be the same so reworked the parameters again

--- Functions/make_scalefreeparameters.m
+++ Functions/make_scalefreeparameters.m
@@ -1,29 +1,28 @@
 function scalefreepars = make_scalefreeparameters(pars, degree, kmin, kmax)
     scalefreepars = pars;
     % Make the necessery parameters for the scalefree networks
-    switch nargin
-        case 2
-            kmin = round(pars.N*3/20);
-            kmax = round(pars.N*2/5);
-        case 3
-            kmax = round(pars.N*2/5);
-    end
+    if nargin < 1; error('Not enough input arguments'); end
+    if nargin < 2; degree = 3; end
+    if nargin < 3; kmin = round(pars.N*3/20); end
+    if nargin < 4; kmax = round(pars.N*2/5); end
+   
     if degree < 2
-        warning('Scale free networks do not exist for degress less than 2');
+        error('Scale free networks do not exist for degress less than 2');
     end
+    
     scalefreepars.kmin = kmin;
     scalefreepars.kmax = kmax;
     scalefreepars.degree = degree;
     
-    scalefreepars.P = @(x) scalefreepdf(x, pars.N, scalefreepars.degree, kmin, kmax);
-    scalefreepars.degrees_in = randsample(kmin:kmax, pars.N, true, scalefreepars.P(kmin:kmax))';
-    if max(scalefreepars.degrees_in) > pars.N - 1
-        disp(['Setting higher degrees to ', num2str(pars.N-1)]);
-        scalefreepars.degrees_in(randompars.degrees_in > pars.N - 1) = pars.N - 1;
+    scalefreepars.P = @(x) scalefreepdf(x, pars.N, degree, kmin, kmax);
+    scalefreepars.degrees_i = randsample(kmin:kmax, pars.N, true, scalefreepars.P(kmin:kmax))';
+    
+    if max(scalefreepars.degrees_i) > pars.N - 1
+        disp(['Setting higher in-degrees to ', num2str(pars.N-1)]);
+        scalefreepars.degrees_i(scalefreepars.degrees_i > pars.N - 1) = pars.N - 1;
     end
-    scalefreepars.degrees_out = scalefreepars.degrees_in(randperm(pars.N));
-
+    scalefreepars.degrees_o = scalefreepars.degrees_i(randperm(pars.N));
+ 
     fsolveoptions = optimset('Display','off');
     scalefreepars.meandegree = fsolve(@(z) scalefreepars.P(z) - mean(scalefreepars.P(kmin:kmax)), kmin, fsolveoptions);
 end
-
--- Tests/OAtest.m
+++ Tests/OAtest.m
@@ -14,23 +14,121 @@ export = true;
 tnow = 0; tend = 10;
 h = 0.005;
 
-pars.N = 5000;
+pars.N = 50;
 pars.a_n = 0.666666666666666666667;
 pars.eta0 = 0.5; pars.delta = 0.7; pars.K = 2;
 
 seed = 2; rng(seed);
 IC = - pi/2 * ones(pars.N, 1);
 
+% Old version:
 sfpars = make_scalefreeparameters(pars, 3);
 sfpars = prepareOAparameters(sfpars);
 
 % The OA mean field theory:
-options = odeset('RelTol', 1.0e-6,'AbsTol', 1.0e-6);
+[TOA, ZOA] = OA_simulatenetwork(tnow, tend, -1i, sfpars);
+plot(TOA, abs(ZOA), 'k')
 
+% New version: a simulation per (k_in, k_out)
+sfpars = make_scalefreeparameters2(pars, 3);
+sfpars = prepareOAparameters2(sfpars);
+
+% The OA mean field theory:
 [TOA, ZOA] = OA_simulatenetwork2(tnow, tend, -1i, sfpars);
+plot(TOA, abs(ZOA), 'r')
 
+%% Testing the distributions
+pars.N = 5000;
+test = make_scalefreeparameters2(pars, true, 3.2, 3, [500, 1500], [1700, 2000])
+
+figure; hold on;
+x = test.krange_i(1):test.krange_i(2);
+histogram(test.degrees_i, 'Normalization', 'pdf')
+plot(x, test.P_i(x)/pars.N, 'LineWidth', 2)
+
+x = test.krange_o(1):test.krange_o(2);
+histogram(test.degrees_o, 'Normalization', 'pdf')
+plot(x, test.P_o(x)/pars.N, 'LineWidth', 2)
+
+% assert(sum(test.degrees_i) == sum(test.degrees_o))
 
 %% Functions
+function scalefreepars = make_scalefreeparameters2(pars, shouldbesame, degree_i, degree_o, krange_i, krange_o)
+    scalefreepars = pars;
+    fsolveoptions = optimset('Display','off');
+    % Make the necessery parameters for the scalefree networks
+    if nargin < 1; error('Not enough input arguments'); end
+    if nargin < 2; shouldbesame = false; end
+    if nargin < 3; degree_i = 3; end
+    if nargin < 4; degree_o = degree_i; end
+    if nargin < 5; krange_i = [round(pars.N*3/20); round(pars.N*2/5)]; end
+    if nargin < 6; krange_o = krange_i; end
+   
+    if degree_i < 2 || degree_o < 2
+        error('Scale free networks do not exist for degress less than 2');
+    end
+    
+    if degree_i == degree_o && krange_i(1) == krange_o(1) && krange_i(2) == krange_o(2)
+        allequal = true;
+    else
+        allequal = false;
+    end
+    
+    scalefreepars.krange_i = krange_i;
+    scalefreepars.krange_o = krange_o;
+    scalefreepars.degree_i = degree_i;
+    scalefreepars.degree_o = degree_i;
+    
+    scalefreepars.P_i = @(x) scalefreepdf(x, pars.N, degree_i, krange_i(1), krange_i(2));
+    scalefreepars.degrees_i = randsample(krange_i(1):krange_i(2), pars.N, true, scalefreepars.P_i(krange_i(1):krange_i(2)))';
+    
+    if allequal
+        scalefreepars.P_o = scalefreepars.P_i;
+        scalefreepars.degrees_o = scalefreepars.degrees_i(randperm(pars.N));
+    else
+        scalefreepars.P_o = @(x) scalefreepdf(x, pars.N, degree_o, krange_o(1), krange_o(2));
+        scalefreepars.degrees_o = randsample(krange_o(1):krange_o(2), pars.N, true, scalefreepars.P_o(krange_o(1):krange_o(2)))';
+    end
+    
+    % Assert maximum degree
+    if max(scalefreepars.degrees_i) > pars.N - 1
+        disp(['Setting higher in-degrees to ', num2str(pars.N-1)]);
+        scalefreepars.degrees_i(scalefreepars.degrees_i > pars.N - 1) = pars.N - 1;
+    end
+    if max(scalefreepars.degrees_o) > pars.N - 1
+        disp(['Setting higher in-degrees to ', num2str(pars.N-1)]);
+        scalefreepars.degrees_o(scalefreepars.degrees_o > pars.N - 1) = pars.N - 1;
+    end
+    
+    % Assert numer of links is equal
+    if shouldbesame == true && sum(scalefreepars.degrees_i) ~= sum(scalefreepars.degrees_o)
+        d_i_n = sum(scalefreepars.degrees_i);
+        d_o_n = sum(scalefreepars.degrees_o);
+        if d_i_n > d_o_n
+            ntoclear = d_i_n - d_o_n
+            if fix(ntoclear/pars.N) > 0
+                scalefreepars.degrees_i = scalefreepars.degrees_i - fix(ntoclear/pars.N);
+            end
+            idx = randperm(pars.N,rem(ntoclear,pars.N));
+            scalefreepars.degrees_i(idx) = scalefreepars.degrees_i(idx) - 1;
+        else
+            ntoclear = d_o_n - d_i_n
+            if fix(ntoclear/pars.N) > 0
+                scalefreepars.degrees_o = scalefreepars.degrees_o - fix(ntoclear/pars.N);
+            end
+            idx = randperm(pars.N,rem(ntoclear,pars.N));
+            scalefreepars.degrees_o(idx) = scalefreepars.degrees_o(idx) - 1;
+        end
+    end
+    
+    scalefreepars.meandegree_i = fsolve(@(z) scalefreepars.P_i(z) - mean(scalefreepars.P_i(krange_i(1):krange_i(2))), krange_i(1), fsolveoptions);
+    if allequal
+        scalefreepars.meandegree_o = scalefreepars.meandegree_i;
+    else
+        scalefreepars.meandegree_o = fsolve(@(z) scalefreepars.P_o(z) - mean(scalefreepars.P_o(krange_o(1):krange_o(2))), krange_o(1), fsolveoptions);
+    end
+end
+
 function p = prepareOAparameters2(p)
     p.k = unique(p.degrees_in);
     p.l = numel(p.k);
