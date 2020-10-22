363a2767a83af3d448510c6d853420e8707bca6c
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-22 21:25:40 +0200

    adjustements

--- Mean Field Reductions/prepareOAparameters.m
+++ Mean Field Reductions/prepareOAparameters.m
@@ -4,7 +4,7 @@ function p = prepareOAparameters(p)
     p.k_o = unique(p.degrees_o, 'stable');
     p.OA = zeros(p.l, p.l);
     for i = 1:p.l
-        p.OA(i, :) = p.P(p.k).*assortativity(p.k, 0, 0, p.k_o(i), p.N, p.meandegree, 0)/p.meandegree;
+        p.OA(i, :) = p.P(p.k).*assortativity(p.k_o, 0, 0, p.k_o(i), p.N, p.meandegree, 0)/p.meandegree;
         
 %good:         p.OA(i, :) = p.P(p.k_o).*assortativity(p.k, p.k_o, ks, p.k_o, p.N, p.meandegree, 0)/p.meandegree;
 %better:         p.OA(i, :) = p.P(p.k(i)).*assortativity(p.k(i), 0, 0, p.k_o, p.N, p.meandegree, 0)/p.meandegree;
