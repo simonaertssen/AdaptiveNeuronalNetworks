7fa8482abe80fc76462ae23d4b1b3608a1cd4ecd
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-22 21:36:22 +0200

    Try the last

--- Full scale simulations/Simulate_with_adjacency.m
+++ Full scale simulations/Simulate_with_adjacency.m
@@ -22,14 +22,14 @@ initarray = make_GPUhandle();
 
 %% Theta model parameters:
 tnow = 0; tend = 10;
-h = 0.005;
+h = 0.001;
 
-pars.N = 10000;
+pars.N = 20000;
 pars.a_n = 0.666666666666666666667;
 pars.eta0 = 10.75; pars.delta = 0.5; pars.K = -9;
 
 seed = 2; rng(seed);
-IC = wrapToPi(randn(pars.N, 1)*1.3);
+IC = wrapToPi(randn(pars.N, 1)*1.2);
 
 pars.e = randcauchy(seed, pars.eta0, pars.delta, pars.N);
 odeoptions = odeset('RelTol', 1.0e-8,'AbsTol', 1.0e-8);
