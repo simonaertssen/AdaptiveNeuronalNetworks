1826742ca701a9b736312cb108c7412cb91bbcaf
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-23 15:40:26 +0200

    tried to change the simulation to have OAIC created elsewehere but that is not necessary

--- Functions/OA_simulatenetwork.m
+++ Functions/OA_simulatenetwork.m
@@ -2,7 +2,7 @@ function [TOA, ZOA, b] = OA_simulatenetwork(tnow, tend, IC, p, odeoptions)
     if nargin < 5
         odeoptions = odeset('RelTol', 1.0e-12,'AbsTol', 1.0e-12);
     end
-
+    
     if numel(IC) > 1
         OAIC = zeros(1,p.l);
         for i = 1:p.l
@@ -13,7 +13,7 @@ function [TOA, ZOA, b] = OA_simulatenetwork(tnow, tend, IC, p, odeoptions)
     else
         error('IC might be wrong?')
     end
-
+    
     [TOA, b] = ode45(@(t,x) MFROA(t,x,p), [tnow, tend], gather(OAIC), odeoptions);
     ZOA = b*p.P(p.k)/p.N;
 end
