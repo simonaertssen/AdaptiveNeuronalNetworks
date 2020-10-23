77c8e2293a26fa057a52db94a983b3a1e71f6705
Simon Aertssen <Simonaertssen@gmail.com>
2020-10-23 10:51:57 +0200

    Integer degrees

--- Functions/make_lognormparameters.m
+++ Functions/make_lognormparameters.m
@@ -8,13 +8,14 @@ function lognormpars = make_lognormparameters(pars, mu, sigma, kmin)
     
     mean = exp(mu + sigma*sigma/2);
     lognormpars.kmin = kmin;
-    lognormpars.degrees_i = kmin + lognrnd(mu, sigma, [pars.N,1]);
+    lognormpars.degrees_i = kmin + round(lognrnd(mu, sigma, [pars.N,1]));
     % Set higher degrees to N - 1:
     if max(lognormpars.degrees_i) > pars.N - 1
         disp(['Setting higher degrees to ', num2str(pars.N-1)]);
         lognormpars.degrees_i(lognormpars.degrees_i > pars.N - 1) = pars.N - 1;
     end
     lognormpars.degrees_o = lognormpars.degrees_i(randperm(pars.N));
+    
     normalisation = pars.N/sum(lognpdf(0:pars.N, mu, sigma));
     lognormpars.P = @(x) lognpdf(x-kmin, mu, sigma)*normalisation;  
     lognormpars.meandegree = kmin + mean;
