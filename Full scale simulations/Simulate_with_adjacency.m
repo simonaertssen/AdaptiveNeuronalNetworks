6843c8dac9c3c44359429752c297f8d860e1654c
Simon Aertssen <Simonaertssen@gmail.com>
2020-11-06 15:49:50 +0100

    try some other ICs

--- Full scale simulations/Simulate_with_adjacency.m
+++ Full scale simulations/Simulate_with_adjacency.m
@@ -156,7 +156,7 @@ disp('Full scale test done')
 % The OA mean field theory:
 sfpars = prepareOAparameters(sfpars);
 z0 = map_thetatozoa(gather(thetasfull(:,1)), sfpars);
-z0 = gather(thetasfull(:,1))*ones(sfpars.Mk,1);
+z0 = orderparameter(IC)*ones(sfpars.Mk,1);
 [TOA, ZOA] = OA_simulatenetwork(tnow, tend, z0, sfpars, odeoptions);
 disp('OA mean field test done')
 
