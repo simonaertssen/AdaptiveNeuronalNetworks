98c2a6c7ab3e2ee687db7b34c0a84a15d3aaaef8
Simon Aertssen <Simonaertssen@gmail.com>
2020-11-17 16:06:31 +0100

    good results for learning without ss

--- Full scale simulations/Exploring_synaptic_plasticity.m
+++ Full scale simulations/Exploring_synaptic_plasticity.m
@@ -22,7 +22,7 @@ end
 initarray = make_GPUhandle();
 
 %% Theta model parameters:
-h = 0.005; tnow = h; tend = 80;
+h = 0.005; tnow = h; tend = 100;
 
 pars.N = 100;
 pars.a_n = 0.666666666666666666667;
@@ -31,26 +31,43 @@ pars.eta0 = 10.75; pars.delta = 0.5; pars.K = -9;
 seed = 1; rng(seed);
 IC = randn(pars.N,1);
 
-pars.e = randcauchy(seed, pars.eta0, pars.delta, pars.N);
+pars.e = 0; %randcauchy(seed, pars.eta0, pars.delta, pars.N);
 
-%% Test simulation:
-clc
-[t, thetas_full, K, Kmeans, info] = DOPRI_simulatenetwork_adaptive(tnow,tend,IC,h,pars,@Waddington2014Window);
-drawthetas = spikesNaN(thetas_full);
+%% Results without synaptic scaling:
+f_noSS = figure('Renderer', 'painters', 'Position', [50, 50, 800, 300]); hold on; box on;
 
-%% Plot results
-figure; hold on; box on;
-xlim([tnow, tend]); ylim([-pi, pi])
-xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
+winnames = ["Kempter1999Window", "Song2000Window", "ChrolCannon2012Window", "Waddington2014Window"];
+colors = ["#0072BD", "#D95319", "#77AC30", "#A2142F"];
+for i = 1:4
+    name = winnames(i);
+    plastopts = struct('SP', true, 'window', str2func(name), 'SS', false, 'IP', false);
+    [t, thetas_full, K, Kmeans] = DOPRI_simulatenetwork_adaptive(tnow,tend,IC,h,pars,plastopts);
+    drawthetas = spikesNaN(thetas_full);
+    
+    imrow(i) = subplot(2,2,i);
+    xlim([tnow, tend]); 
+    
+    yyaxis left
+    plot(t, drawthetas, '-', 'LineWidth', 1.5, 'Color', [0, 0, 1, 0.01], 'HandleVisibility', 'off')
+    ylabel('$\theta_i$','Interpreter','latex', 'FontSize', labelfont)
+    ylim([-pi, pi]); 
 
-yyaxis right
-plot(t, Kmeans, '-k', 'LineWidth', 2)
-ylabel('$\langle k \rangle$','Interpreter','latex', 'FontSize', labelfont)
+    yyaxis right
+    plot(t, Kmeans, 'LineWidth', 2, 'Color', colors(i))
+    ylabel('$\langle k \rangle$','Interpreter','latex', 'FontSize', labelfont)
+    
+    name = char(name);
+    legend(sprintf('$$W(t)_%s$$', name(1)), 'Location', 'southwest', 'Interpreter', 'latex', 'FontSize', labelfont)
 
-yyaxis left
-% plot(t, info, '-k', 'LineWidth', 0.1)
-plot(t, drawthetas, '-', 'LineWidth', 1.5, 'Color', [0, 0, 1, 0.01])
-ylabel('$\theta_i$','Interpreter','latex', 'FontSize', labelfont)
+    ax = gca; ax.YAxis(1).Color = [0, 0, 1]; ax.YAxis(2).Color = 'k';
+    xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
+end
 
-ax = gca; ax.YAxis(1).Color = [0, 0, 1]; ax.YAxis(2).Color = 'k';
+print(f_noSS, '../Figures/LearningWithoutScaling.png', '-dpng', '-r300')
+close(f_noSS)
+
+
+
+
+
 
