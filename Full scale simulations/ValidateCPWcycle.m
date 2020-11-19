9fe95c7ac508b325c334b2771ca2d78713cc1c31
Simon Aertssen <Simonaertssen@gmail.com>
2020-11-19 22:00:08 +0100

    what does an undisturbed pdf do?

--- Full scale simulations/ValidateCPWcycle.m
+++ Full scale simulations/ValidateCPWcycle.m
@@ -186,7 +186,7 @@ odeoptions = odeset('RelTol', 1.0e-9,'AbsTol', 1.0e-9);
 % The full scale simulation using the adjacency matrix:
 degree = 3;
 IC = wrapToPi(randn(pars.N, 1)*1.2);
-sfpars = make_scalefreeparameters(pars, degree, 100, 1000);
+sfpars = make_scalefreeparameters(pars, degree);
 [~, thetasfull] = DOPRI_simulatenetwork(tnow,tend,IC,h,sfpars);
 zfull = orderparameter(thetasfull);
 ts = findlimitcycle(abs(zfull));
