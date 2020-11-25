f344313297a61be1c96f803c41d9d54d7fcf8fcd
Simon Aertssen <Simonaertssen@gmail.com>
2020-11-25 17:53:09 +0100

    made figures more visible from the pdf

--- Mean Field Reductions/PhaseSpace.m
+++ Mean Field Reductions/PhaseSpace.m
@@ -12,7 +12,9 @@ rect = [-50 50 800 800];
 cm = [1,0,0; 0, 0.7410, 0.4470; 0, 0.4470, 0.6410];
 
 titlefont = 15;
-labelfont = 13;
+labelfont = 30;
+linewidth = 4;
+arrowst = 12;
 
 fsolveoptions = optimset('Display','off');
 odeoptions = odeset('RelTol', 1.0e-8,'AbsTol', 1.0e-8);
@@ -69,17 +71,17 @@ quiver(X, Y, real(complex), imag(complex), 'color', cm(2,:))
 complex = MFR(0, X + 1i*Y, pars);
 startx = 0.8*cos( -pi/5:pi/5:pi); starty = 0.8*sin(-pi/5:pi/5:pi);
 startx(2) = []; starty(2) = [];
-scatter(startx, starty, 50, cm(3,:), 'filled', 'o', 'LineWidth',2);
-s = streamline(X, Y, real(complex), imag(complex), startx, starty, [0.05,1000]);
-set(s, 'color', cm(3,:), 'LineWidth', 2);
+scatter(startx, starty, 50, cm(3,:), 'filled', 'o', 'LineWidth', linewidth);
+s = streamline(X, Y, real(complex), imag(complex), startx, starty, [0.05,700]);
+set(s, 'color', cm(3,:), 'LineWidth', linewidth);
 for i = 1:6
     dist = (s(i).XData - eqpt(1)).^2 + (s(i).YData - eqpt(2)).^2 - 0.02;
     dist(end) = -1;
     idx = find(dist < 0, 1, 'first');
     s(i).XData = s(i).XData(1:idx);
     s(i).YData = s(i).YData(1:idx);
-    plot_arrow(s(i).XData(end-8), s(i).YData(end-8), s(i).XData(end), s(i).YData(end),'linewidth', 2, ...
-    'color', cm(3,:),'facecolor', cm(3,:),'edgecolor', cm(3,:), 'headwidth',0.7,'headheight',3);
+    plot_arrow(s(i).XData(end-arrowst), s(i).YData(end-arrowst), s(i).XData(end), s(i).YData(end),'linewidth', 3, ...
+    'color', cm(3,:),'facecolor', cm(3,:),'edgecolor', cm(3,:), 'headwidth',2,'headheight',6);
 end
 
 phasespaceplot();
@@ -90,21 +92,21 @@ index = 1;
 for i = 1:2
     lambda = D(i,i);
     bef = 0.04;
-    endl = 0.075;
+    endl = 0.095;
     draw = 0.75*endl*lambda;
     if lambda < 0
-        plot_arrow(eqpt(1) + endl*V(1,index), eqpt(2) + endl*V(2,index), eqpt(1) + bef*V(1,index), eqpt(2) + bef*V(2,index), 'headwidth',0.7,'headheight',3); 
-        plot_arrow(eqpt(1) - endl*V(1,index), eqpt(2) - endl*V(2,index), eqpt(1) - bef*V(1,index), eqpt(2) - bef*V(2,index), 'headwidth',0.7,'headheight',3); 
-        plot([eqpt(1) - draw*V(1,index), eqpt(1) + draw*V(1,index)], [eqpt(2) - draw*V(2,index), eqpt(2) + draw*V(2,index)], 'k', 'LineWidth', 2);
+        plot_arrow(eqpt(1) + endl*V(1,index), eqpt(2) + endl*V(2,index), eqpt(1) + bef*V(1,index), eqpt(2) + bef*V(2,index), 'headwidth',0.9,'headheight',6); 
+        plot_arrow(eqpt(1) - endl*V(1,index), eqpt(2) - endl*V(2,index), eqpt(1) - bef*V(1,index), eqpt(2) - bef*V(2,index), 'headwidth',0.9,'headheight',6); 
+        plot([eqpt(1) - draw*V(1,index), eqpt(1) + draw*V(1,index)], [eqpt(2) - draw*V(2,index), eqpt(2) + draw*V(2,index)], 'k', 'LineWidth', linewidth);
     end
     index = index + 1;
 end
 scatter(eqpt(1), eqpt(2), 150, 'or', 'filled')
 
 % End figure:
-set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]);
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]);
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_MFRPSR, '../Figures/MFRPSR.png', '-dpng', '-r300')
 close(f_MFRPSR)
 
@@ -125,10 +127,10 @@ quiver(X, Y, real(complex), imag(complex), 'color', cm(2,:))
 % Lines:
 complex = MFR(0, X + 1i*Y, pars);
 startx = 1; starty = 0;
-scatter(startx, starty, 50, cm(3,:), 'filled', 'o', 'LineWidth',2);% 'color', cm(3,:));
+scatter(startx, starty, 50, cm(3,:), 'filled', 'o', 'LineWidth', linewidth);% 'color', cm(3,:));
 s = streamline(X, Y, real(complex), imag(complex), startx, starty, [0.05,1550]);
-set(s, 'color', cm(3,:), 'LineWidth', 2);
-plot_arrow(s.XData(end-8), s.YData(end-8), s.XData(end), s.YData(end),'linewidth', 2, ...
+set(s, 'color', cm(3,:), 'LineWidth', linewidth);
+plot_arrow(s.XData(end-arrowst), s.YData(end-arrowst), s.XData(end), s.YData(end),'linewidth', 2, ...
     'color', cm(3,:),'facecolor', cm(3,:),'edgecolor', cm(3,:), 'headwidth',0.7,'headheight',3);
 
 phasespaceplot();
@@ -138,9 +140,9 @@ phasespaceplot();
 scatter(eqpt(1), eqpt(2), 150, 'or', 'filled')
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_MFRPSS, '../Figures/MFRPSS.png', '-dpng', '-r300')
 close(f_MFRPSS)
 
@@ -162,9 +164,9 @@ quiver(X, Y, real(complex), imag(complex), 'color', cm(2,:))
 % Lines:
 complex = MFR(0, X + 1i*Y, pars);
 startx = [0, 0, -0.6, -0.8]; starty = [-1, -0.8, 0.4, 0.2];
-scatter(startx, starty, 50, cm(3,:), 'filled', 'o', 'LineWidth',2);
+scatter(startx, starty, 50, cm(3,:), 'filled', 'o', 'LineWidth', linewidth);
 s = streamline(X, Y, real(complex), imag(complex), startx, starty, [0.01,9000]);
-set(s, 'color', cm(3,:), 'LineWidth', 2);
+set(s, 'color', cm(3,:), 'LineWidth', linewidth);
 
 % Streamlines yield inexact results?
 for i = 1:4
@@ -173,7 +175,7 @@ for i = 1:4
     idx = find(dist < 0, 1, 'first');
     s(i).XData = s(i).XData(1:idx);
     s(i).YData = s(i).YData(1:idx);
-    plot_arrow(s(i).XData(end-40), s(i).YData(end-40), s(i).XData(end), s(i).YData(end),'linewidth', 2, ...
+    plot_arrow(s(i).XData(end-5*arrowst), s(i).YData(end-5*arrowst), s(i).XData(end), s(i).YData(end),'linewidth', 2, ...
     'color', cm(3,:),'facecolor', cm(3,:),'edgecolor', cm(3,:), 'headwidth',0.7,'headheight',3);     
 end
 
@@ -183,7 +185,7 @@ Z0 = -0.2731 - 0.0092*1i;
 Z = flip(Z(round(numel(T)*0.97):end,:));
 [~, pksloc] = findpeaks(abs(Z),'MinPeakDistance',100);
 idx = pksloc(1):pksloc(3);
-plot(real(Z(idx)), imag(Z(idx)), '-', 'LineWidth', 2, 'Color', cm(3,:));
+plot(real(Z(idx)), imag(Z(idx)), '-', 'LineWidth', linewidth, 'Color', cm(3,:));
 plot_arrow(real(Z(end)), imag(Z(end)), real(Z(end-2)), imag(Z(end-2)),'linewidth', 2, ...
     'color', cm(3,:),'facecolor', cm(3,:),'edgecolor', cm(3,:), 'headwidth',0.4,'headheight',1);
 
@@ -198,16 +200,16 @@ for eqpointindex = 1:2
     for i = 1:2
         lambda = D(i,i);
         bef = 0.04;
-        endl = 0.075;
+        endl = 0.095;
         draw = 0.5*endl*lambda;
         if lambda < 0
             plot_arrow(eqpt(1) + endl*V(1,index), eqpt(2) + endl*V(2,index), eqpt(1) + bef*V(1,index), eqpt(2) + bef*V(2,index), 'headwidth',0.7,'headheight',3);
             plot_arrow(eqpt(1) - endl*V(1,index), eqpt(2) - endl*V(2,index), eqpt(1) - bef*V(1,index), eqpt(2) - bef*V(2,index), 'headwidth',0.7,'headheight',3);
-            plot([eqpt(1) - draw*V(1,index), eqpt(1) + draw*V(1,index)], [eqpt(2) - draw*V(2,index), eqpt(2) + draw*V(2,index)], 'k', 'LineWidth', 2);
+            plot([eqpt(1) - draw*V(1,index), eqpt(1) + draw*V(1,index)], [eqpt(2) - draw*V(2,index), eqpt(2) + draw*V(2,index)], 'k', 'LineWidth', linewidth);
         else
             plot_arrow(eqpt(1) + endl*V(1,index), eqpt(2) + endl*V(2,index),eqpt(1) + draw*V(1,index), eqpt(2) + draw*V(2,index),'headwidth',0.7,'headheight',3);
             plot_arrow(eqpt(1) - endl*V(1,index), eqpt(2) - endl*V(2,index),eqpt(1) - draw*V(1,index), eqpt(2) - draw*V(2,index),'headwidth',0.7,'headheight',3);
-            plot([eqpt(1) - draw*V(1,index), eqpt(1) + draw*V(1,index)], [eqpt(2) - draw*V(2,index), eqpt(2) + draw*V(2,index)], 'k', 'LineWidth', 2);
+            plot([eqpt(1) - draw*V(1,index), eqpt(1) + draw*V(1,index)], [eqpt(2) - draw*V(2,index), eqpt(2) + draw*V(2,index)], 'k', 'LineWidth', linewidth);
         end
         index = index + 1;
     end
@@ -219,14 +221,14 @@ end
 t = linspace(0, 2*pi + 0.3, 100);
 radius = t/max(t)/10;
 curve = [eqpt(1) + radius.*cos(t); eqpt(2) + radius.*sin(t)];
-plot(curve(1,:), curve(2,:), 'k', 'LineWidth', 2)
+plot(curve(1,:), curve(2,:), 'k', 'LineWidth', linewidth)
 scatter(eqpt(1), eqpt(2), 150, 'or', 'filled')
-plot_arrow(curve(1,end-6), curve(2,end-6), curve(1,end), curve(2,end), 'headwidth',0.6,'headheight',1.5);
+plot_arrow(curve(1,end-6), curve(2,end-6), curve(1,end), curve(2,end), 'headwidth',1,'headheight',1.5);
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_MFRCPW, '../Figures/MFRCPW.png', '-dpng', '-r300')
 close(f_MFRCPW)
 
@@ -251,11 +253,11 @@ for i = 1:length(startx)
     OAIC = ones(p.Mk,1)*(startx(i) + starty(i)*1i);
     [~, ZOA] = OA_simulatenetwork(0, tlengths(i), OAIC, p, odeoptions);
 
-    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth',2);% 'color', col);
+    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth', linewidth);% 'color', col);
 
-    Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+    Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
     endline = ZOA(end-3) - ZOA(end);
-    endpoint = ZOA(end) + 0.02*endline/abs(endline);
+    endpoint = ZOA(end) + 0.04*endline/abs(endline);
     plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linewidth', 2, ...
     'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3);   
 end
@@ -270,9 +272,9 @@ J = MFROAJ(eqptb, p);
 phasespaceplot();
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_OARPSR, '../Figures/MFOARPSR_random.png', '-dpng', '-r300')
 close(f_OARPSR)
 
@@ -291,10 +293,10 @@ startx = 1; starty = 0; tlength = 3.4;
 odeoptions = odeset('RelTol', 1.0e-12); odeoptions.backwards = true;
 
 [~, ZOA] = OA_simulatenetwork(0, tlength, ones(p.Mk,1)*(startx + starty*1i), p, odeoptions);
-scatter(startx, starty, 50, col, 'filled', 'o', 'LineWidth',2);% 'color', col);
-Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+scatter(startx, starty, 50, col, 'filled', 'o', 'LineWidth', linewidth);% 'color', col);
+Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
 endline = ZOA(end-3) - ZOA(end);
-endpoint = ZOA(end) + 0.02*endline/abs(endline);
+endpoint = ZOA(end) + 0.04*endline/abs(endline);
 plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linewidth', 2, ...
 'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3); 
 
@@ -306,9 +308,9 @@ eqptZ = eqptb'*p.P(p.k)/p.N;
 scatter(real(eqptZ), imag(eqptZ), 150, 'or', 'filled')
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_OARPSS, '../Figures/MFOARPSS_random.png', '-dpng', '-r300')
 close(f_OARPSS)
 
@@ -339,22 +341,22 @@ for i = 1:length(startx)-1
     OAIC = ones(p.Mk,1)*(startx(i) + starty(i)*1i);
     [~, ZOA] = OA_simulatenetwork(0, tlengths(i), OAIC, p);
 
-    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth',2);% 'color', col);
+    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth', linewidth);% 'color', col);
 
-    plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+    plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
     endline = ZOA(end-3) - ZOA(end);
-    endpoint = ZOA(end) + 0.02*endline/abs(endline);
+    endpoint = ZOA(end) + 0.04*endline/abs(endline);
     plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linewidth', 2, ...
     'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3);   
 end
 
 % Limit cycle:
 [T, ZOA] = OA_simulatenetwork(0, 100, ones(p.Mk,1)*(-0.73*1i), p);
-% plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+% plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
 ZOA = flip(ZOA(round(numel(T)*0.9):end,:));
 [~, pksloc] = findpeaks(abs(ZOA),'MinPeakDistance',100);
 idx = pksloc(1):pksloc(3);
-plot(real(ZOA(idx)), imag(ZOA(idx)), '-', 'LineWidth', 2, 'Color', col);
+plot(real(ZOA(idx)), imag(ZOA(idx)), '-', 'LineWidth', linewidth, 'Color', col);
 plot_arrow(real(ZOA(end)), imag(ZOA(end)), real(ZOA(end-2)), imag(ZOA(end-2)),'linewidth', 2, ...
     'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3);
 
@@ -366,11 +368,11 @@ eqptZ = eqptb'*p.P(p.k)/p.N;
 scatter(real(eqptZ), imag(eqptZ), 150, 'or', 'filled')
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-% print(f_OARCPW, '../Figures/MFOARCPW_random.png', '-dpng', '-r300')
-% close(f_OARCPW)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+print(f_OARCPW, '../Figures/MFOARCPW_random.png', '-dpng', '-r300')
+close(f_OARCPW)
 
 
 %% 7. OA scalefree phase space: PSR
@@ -393,11 +395,11 @@ for i = 1:length(startx)
     OAIC = ones(p.Mk,1)*(startx(i) + starty(i)*1i);
     [~, ZOA] = OA_simulatenetwork(0, tlengths(i), OAIC, p, odeoptions);
 
-    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth',2);% 'color', col);
+    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth', linewidth);% 'color', col);
 
-    Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+    Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
     endline = ZOA(end-3) - ZOA(end);
-    endpoint = ZOA(end) + 0.02*endline/abs(endline);
+    endpoint = ZOA(end) + 0.04*endline/abs(endline);
     plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linewidth', 2, ...
     'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3);   
 end
@@ -412,9 +414,9 @@ J = MFROAJ(eqptb, p);
 phasespaceplot();
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_OARPSR, '../Figures/MFOARPSR_scalefree.png', '-dpng', '-r300')
 close(f_OARPSR)
 
@@ -433,10 +435,10 @@ startx = 1; starty = 0; tlength = 3.4;
 odeoptions = odeset('RelTol', 1.0e-12); odeoptions.backwards = true;
 
 [~, ZOA] = OA_simulatenetwork(0, tlength, ones(p.Mk,1)*(startx + starty*1i), p, odeoptions);
-scatter(startx, starty, 50, col, 'filled', 'o', 'LineWidth',2);% 'color', col);
-Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+scatter(startx, starty, 50, col, 'filled', 'o', 'LineWidth', linewidth);% 'color', col);
+Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
 endline = ZOA(end-3) - ZOA(end);
-endpoint = ZOA(end) + 0.02*endline/abs(endline);
+endpoint = ZOA(end) + 0.04*endline/abs(endline);
 plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linewidth', 2, ...
 'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3); 
 
@@ -448,9 +450,9 @@ eqptZ = eqptb'*p.P(p.k)/p.N;
 scatter(real(eqptZ), imag(eqptZ), 150, 'or', 'filled')
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_OARPSS, '../Figures/MFOARPSS_scalefree.png', '-dpng', '-r300')
 close(f_OARPSS)
 
@@ -475,23 +477,23 @@ for i = 1:length(startx)
     OAIC = ones(p.Mk,1)*(startx(i) + starty(i)*1i);
     [~, ZOA] = OA_simulatenetwork(0, tlengths(i), OAIC, p, odeoptions);
     
-    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth',2);% 'color', col);
+    scatter(startx(i), starty(i), 50, col, 'filled', 'o', 'LineWidth', linewidth);% 'color', col);
 
-    Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+    Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
     endline = ZOA(end-3) - ZOA(end);
-    endpoint = ZOA(end) + 0.02*endline/abs(endline);
+    endpoint = ZOA(end) + 0.04*endline/abs(endline);
     plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linewidth', 2, ...
     'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3);   
 end
 
 % Limit cycle:
 [T, ZOA] = OA_simulatenetwork(0, 100, ones(p.Mk,1)*(-0.2*1i), p, odeoptions);
-% Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', 2, 'color', col);
+% Zplot = plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
 ZOA = flip(ZOA(round(numel(T)*0.9):end,:));
 [~, pksloc] = findpeaks(abs(ZOA),'MinPeakDistance',100);
 idx = pksloc(1):pksloc(3);
-plot(real(ZOA(idx)), imag(ZOA(idx)), '-', 'LineWidth', 2, 'Color', col);
-plot_arrow(real(ZOA(end)), imag(ZOA(end)), real(ZOA(end-2)), imag(ZOA(end-2)),'linewidth', 2, ...
+plot(real(ZOA(idx)), imag(ZOA(idx)), '-', 'LineWidth', linewidth, 'Color', col);
+plot_arrow(real(ZOA(end)), imag(ZOA(end)), real(ZOA(end-3)), imag(ZOA(end-3)),'linewidth', 2, ...
     'color', col,'facecolor', col,'edgecolor', col, 'headwidth',0.7,'headheight',3);
 
 phasespaceplot();
@@ -502,9 +504,9 @@ eqptZ = eqptb'*p.P(p.k)/p.N;
 scatter(real(eqptZ), imag(eqptZ), 150, 'or', 'filled')
 
 % End figure:
-hold off; set(gcf,'color','w'); set(gca,'FontSize',14); xlim([-1,1]); ylim([-1,1]); axis square;
-xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
-ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', 20)
+hold off; set(gcf,'color','w'); xlim([-1,1]); ylim([-1,1]); axis square;
+xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 print(f_OARCPW, '../Figures/MFOARCPW_scalefree.png', '-dpng', '-r300')
 close(f_OARCPW)
 
