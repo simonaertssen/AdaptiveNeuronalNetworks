14c350e2e37c7e214d41dd47203919e6a9f5ec1f
Simon Aertssen <Simonaertssen@gmail.com>
2020-12-02 18:38:13 +0100

    latest results added

--- Full scale simulations/STDPandIP.m
+++ Full scale simulations/STDPandIP.m
@@ -20,19 +20,21 @@ end
 initarray = make_GPUhandle();
 
 %% Theta model parameters:
-h = 0.005; tnow = h; tend = 100;
+h = 0.01; tnow = h; tend = 2000;
 
 pars.N = 100;
 pars.a_n = 0.666666666666666666667;
 seed = 2; rng(seed);
-IC = randn(pars.N,1);
-pars.e = 1; %randcauchy(seed, pars.eta0, pars.delta, pars.N);
+IC = linspace(0, 2*pi - (2*pi)/(pars.N),pars.N)';
+pars.e = 0; %randcauchy(seed, pars.eta0, pars.delta, pars.N);
 
 %% The simulation
 % STDP = struct('window', @Kempter1999Window, 'Kupdate', @(K, W) K + W, 'w_i', 1.0e-5, 'w_o', - 1.0475*1.0e-5);
 STDP = struct('window', @Song2017Window, 'Kupdate', @(K, W) K + K.*W); 
-plastopts = struct('SP', STDP, 'KMAX', 10);
-K0 = initarray(zeros(pars.N) + 0.01*randn(pars.N)); % For the STDPversusIP script
+KMAX = 10;
+plastopts = struct('SP', STDP, 'KMAX', KMAX);
+
+K0 = initarray(rand(pars.N)*2 - 1);
 
 [t, thetas_full, K, Kmeans] = DOPRI_simulatenetwork_adaptive(tnow,tend,IC,h,pars,K0,plastopts);
 drawthetas = spikesNaN(thetas_full);
@@ -45,20 +47,22 @@ f_reproduce = figure('Renderer', 'painters', 'Position', [0, 2000, 300, 1400]); 
 subplot(5,1,1); hold on; axis square; box on;
 title('Simulation results', 'FontSize', titlefont)
 yyaxis left
-rasterplot(t, drawthetas, labelfont);
+rasterplot(t, drawthetas, labelfont, 0.1);
 
 yyaxis right
-plot(t, Kmeans, 'LineWidth', 2, 'Color', "#0072BD")
+plot(t, Kmeans(2,:), 'LineWidth', 2, 'Color', "#0072BD")
 ax = gca; ax.YAxis(1).Color = [0, 0, 1]; ax.YAxis(2).Color = "#0072BD";
 xlabel('$t$','Interpreter','latex', 'FontSize', labelfont)
 ylabel('$\langle k \rangle$','Interpreter','latex', 'FontSize', labelfont)
+
+ax = gca; ax.YAxis(1).Color = [0, 0, 0];
 
 subplot(5,1,2); hold on; axis square; box on;
 title('{ \boldmath $K_{ij} $}', 'Interpreter', 'latex', 'FontSize', titlefont)
 xlim([0, pars.N]); ylim([0, pars.N]);
 xlabel('Presynaptic neuron j', 'FontSize', labelfont)
 ylabel('Postynaptic neuron i', 'FontSize', labelfont)
-imagesc(K); colormap(gray);
+imagesc(K/KMAX); colormap(gray);
 
 subplot(5,1,3); hold on; axis square; axis on; box on;
 histogram(K, 'Normalization', 'pdf')
@@ -68,8 +72,8 @@ ylabel('Density', 'FontSize', labelfont)
 
 subplot(5,1,4); hold on; axis square; box on;
 title('Degree distributions', 'FontSize', titlefont)
-degrees_i = sum(K,2);
-degrees_o = sum(K,1);
+degrees_i = sum(abs(K),2);
+degrees_o = sum(abs(K),1);
 histogram(degrees_i, 'Normalization', 'pdf')
 histogram(degrees_o, 'Normalization', 'pdf')
 legend('$$k^{\rm in}$$', '$$k^{\rm out}$$', 'Interpreter', 'latex', 'FontSize', labelfont)
--- Full scale simulations/rasterplot.m
+++ Full scale simulations/rasterplot.m
@@ -1,8 +1,8 @@
-function fighandle = rasterplot(t, drawthetas, labelfont)
+function fighandle = rasterplot(t, drawthetas, labelfont, ptsze)
     [N, tpts] = size(drawthetas);
     
     [spikesr, spikesc] = ind2sub([N, tpts], find(isnan(drawthetas)));
-    fighandle = scatter(t(spikesc), spikesr, 10, '.k');
+    fighandle = scatter(t(spikesc), spikesr, ptsze, '.k');
     
     ylim([0, N]); xlim([0, t(end)]);
     ylabel('Neuron index', 'FontSize', labelfont)
