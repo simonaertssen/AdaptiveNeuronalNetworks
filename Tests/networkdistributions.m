314c031e374c41c59c5a8a7c9956e6368833766e
Simon Aertssen <Simonaertssen@gmail.com>
2020-12-19 12:15:59 +0100

    making progress on the 2D pdf

--- Tests/networkdistributions.m
+++ Tests/networkdistributions.m
@@ -9,6 +9,8 @@ titlefont = 15;
 
 pars.N = 10000;
 meandegreetoget = 100;
+
+%% Figure handle
 f_1Dpdfs = figure('Renderer', 'painters', 'Position', [50 800 900 400]); box on; hold on;
 
 %% A 1D random network: CORRECT
@@ -85,9 +87,6 @@ xlabel('$$k^{\rm in}$$', 'Interpreter', 'latex', 'FontSize', labelfont)
 ylabel('$$k^{\rm out}$$', 'Interpreter', 'latex', 'FontSize', labelfont)
 pos = plt.Position; plt.Position = [pos(1) + 0.09, pos(2), pos(3), pos(4)];
 
-%% Produce 1D figure
-print(f_1Dpdfs, '../Figures/Distributions/1D.png', '-dpng', '-r300')
-
 %% Test: a 1D lognormal network - inspired by scale-free CORRECT
 % lognormpars = make_lognormparameters(pars, 3, 1, 500);
 % 
@@ -103,6 +102,10 @@ print(f_1Dpdfs, '../Figures/Distributions/1D.png', '-dpng', '-r300')
 % plot(x, lognormpars.P(x)/pars.N);
 % xline(lognormpars.meandegree, 'k--')
 
+%% Produce 1D figure
+print(f_1Dpdfs, '../Figures/Distributions/1D.png', '-dpng', '-r300')
+
+
 %% Now the 2D pdfs:
 %% A 2D fixed degree network / diracnet
 fixeddegreepars = make_fixeddegreeparameters(pars, meandegreetoget);
@@ -110,17 +113,31 @@ fixeddegreepars = make_fixeddegreeparameters(pars, meandegreetoget);
 fixeddegreepars.P2D = @(x,y) fixeddegreepars.P(x) .* fixeddegreepars.P(y) / pars.N;
 
 if sum(fixeddegreepars.P2D(1:pars.N, 1:pars.N), 'all') == pars.N; disp('Sum is correct'); end
+
 %%
 figure; hold on; grid on;
 title('Fixed degree', 'FontSize', titlefont);
-plot3([meandegreetoget, meandegreetoget], [meandegreetoget, meandegreetoget], [0, 2])
-minmax = linspace(meandegreetoget - 10, meandegreetoget + 10, 20);
-histogram2(fixeddegreepars.degrees_i, fixeddegreepars.degrees_o, minmax, minmax, 'Normalization', 'pdf'); % Normal degree vectors from before
-xlabel('$$k^{\rm in}$$', 'Interpreter', 'latex', 'FontSize', labelfont);
-ylabel('$$k^{\rm out}$$', 'Interpreter', 'latex', 'FontSize', labelfont);
+xlabel('\boldmath$k^{\rm in}$', 'Interpreter', 'latex', 'FontSize', labelfont);
+ylabel('\boldmath$k^{\rm out}$', 'Interpreter', 'latex', 'FontSize', labelfont);
+zlabel('Density', 'FontSize', labelfont, 'HorizontalAlignment', 'left');
 view(110, 20)
 
-scatter3(fixeddegreepars.degrees_i, fixeddegreepars.degrees_o, -ones(1, pars.N), 150, '.k');
+% The 3D histogram
+minmax = linspace(meandegreetoget - 10, meandegreetoget + 10, 20);
+xlim([minmax(1), minmax(end)])
+ylim([minmax(1), minmax(end)])
+hist = histogram2(fixeddegreepars.degrees_i, fixeddegreepars.degrees_o, minmax, minmax, 'Normalization', 'pdf','ShowEmptyBins','on'); % Normal degree vectors from before
+hist.FaceAlpha = 0.75;
+
+% The pdf
+plot3([meandegreetoget, meandegreetoget], [meandegreetoget, meandegreetoget], [0, max(hist.Values, [], 'all')], 'k', 'LineWidth', 2)
+
+% % A value of zero-surface
+% [x,y] = meshgrid(minmax(1):minmax(end),minmax(1):minmax(end));
+% surf(x,y,zeros(size(x)),'FaceAlpha',0.5,'EdgeColor','none');
+
+% The 2D scatter plot underneath
+scatter3(fixeddegreepars.degrees_i, fixeddegreepars.degrees_o, -ones(1, pars.N), 200, '.k');
 ax = gca;
 ax.ZTick(ax.ZTick < 0) = [];
 
@@ -128,28 +145,81 @@ ax.ZTick(ax.ZTick < 0) = [];
 %% A 2D random network:
 randompars = make_randomparameters(pars, meandegreetoget/(pars.N - 1));
 
-randompars.P2D = @(x,y) randompars.P(x) / pars.N;
+randompars.P2D = @(x,y) randompars.P(x) .* randompars.P(y) / pars.N;
 
 if sum(randompars.P2D(1:pars.N, 1:pars.N), 'all') == pars.N; disp('Sum is correct'); end
+
 %%
 figure; hold on; grid on;
 title('Random', 'FontSize', titlefont);
-
-minmax = linspace(randompars.meandegree - 3*sqrt(randompars.meandegree), randompars.meandegree + 3*sqrt(randompars.meandegree), 20);
-
-histogram2(randompars.degrees_i, randompars.degrees_o, minmax, minmax, 'Normalization', 'pdf'); % Normal degree vectors from before
-s = findobj(gca,'Type','Surface');
-s.FaceAlpha = 0.65;
-
-x = 70:130;
-P1 = randompars.P(x).*randompars.P(x)/(pars.N^2);
-plot3(x, meandegreetoget*ones(size(x)), P1, 'LineWidth', 4);
-plot3(meandegreetoget*ones(size(x)), x, P1, 'LineWidth', 4);
-
-xlabel('$$k^{\rm in}$$', 'Interpreter', 'latex', 'FontSize', labelfont);
-ylabel('$$k^{\rm out}$$', 'Interpreter', 'latex', 'FontSize', labelfont);
+xlabel('\boldmath$k^{\rm in}$', 'Interpreter', 'latex', 'FontSize', labelfont);
+ylabel('\boldmath$k^{\rm out}$', 'Interpreter', 'latex', 'FontSize', labelfont);
+zlabel('Density', 'FontSize', labelfont, 'HorizontalAlignment', 'left');
 view(110, 20)
-% 
-% scatter3(randompars.degrees_i, randompars.degrees_o, -0.001*ones(1, pars.N), '.k');
-% ax = gca;
-% ax.ZTick(ax.ZTick < 0) = [];
+
+% The 3D histogram
+minmax = linspace(randompars.meandegree - 3*sqrt(randompars.meandegree), randompars.meandegree + 3*sqrt(randompars.meandegree), 16);
+xlim([minmax(1), minmax(end)])
+ylim([minmax(1), minmax(end)])
+hist = histogram2(randompars.degrees_i, randompars.degrees_o, minmax, minmax, 'Normalization', 'pdf', 'FaceAlpha',1,'ShowEmptyBins','on'); % Normal degree vectors from before
+
+% The pdf
+vec = linspace(minmax(1), minmax(end), minmax(end) - minmax(1) + 1);
+[x,y] = meshgrid(vec, vec);
+idx = round(linspace(1, numel(vec), 25));
+surf(x(idx,idx),y(idx,idx),randompars.P2D(x(idx,idx),y(idx,idx))/pars.N,'FaceAlpha',0.5,'EdgeColor','none');
+
+% The 2D scatter plot underneath
+midvalues = 0.5 * (minmax(1:end-1) + minmax(2:end));
+h = pcolor(midvalues,midvalues,hist.Values);
+h.ZData = -max(hist.Values, [], 'all')*ones(size(hist.Values));
+
+ax = gca;
+ax.ZTick(ax.ZTick < 0) = [];
+
+
+%% A 2D scalefree network 
+scalefreepars = make_scalefreeparameters(pars, 3);
+
+scalefreepars.P2D = @(x,y) P2D(x, y, scalefreepars.kmin, scalefreepars.kmax, scalefreepars.degree, scalefreepars.N)/pars.N;
+
+if sum(scalefreepars.P2D(1:pars.N, 1:pars.N), 'all') == pars.N; disp('Sum is correct'); end
+%%
+figure; hold on; grid on;
+title('Scale-free', 'FontSize', titlefont);
+xlabel('\boldmath$k^{\rm in}$', 'Interpreter', 'latex', 'FontSize', labelfont);
+ylabel('\boldmath$k^{\rm out}$', 'Interpreter', 'latex', 'FontSize', labelfont);
+zlabel('Density', 'FontSize', labelfont, 'HorizontalAlignment', 'left');
+view(110, 20)
+
+% The 3D histogram
+minmax = linspace(scalefreepars.kmin, scalefreepars.kmax, 16);
+hist = histogram2(scalefreepars.degrees_i, scalefreepars.degrees_o, minmax, minmax, 'Normalization', 'pdf'); % Normal degree vectors from before
+xlim([minmax(1), minmax(end)])
+ylim([minmax(1), minmax(end)])
+
+% The pdf
+minmax = linspace(scalefreepars.kmin, scalefreepars.kmax, scalefreepars.kmax-scalefreepars.kmin+1);
+[x,y] = meshgrid(minmax, minmax);
+idx = round(linspace(1, numel(minmax), 25));
+surf(x(idx,idx),y(idx,idx),scalefreepars.P2D(x(idx,idx),y(idx,idx)),'FaceAlpha',0.5,'EdgeColor','none');
+
+% The 2D scatter plot underneath
+scatter3(scalefreepars.degrees_i, scalefreepars.degrees_o, -ones(1, pars.N), 150, '.k');
+h = imagesc(xl,yl,N_pcolor);
+ax = gca;
+ax.ZTick(ax.ZTick < 0) = [];
+
+%% Produce 2D figure
+print(f_2Dpdfs, '../Figures/Distributions/2D.png', '-dpng', '-r300')
+
+
+%% Functions:
+function P = P2D(X,Y, kmin, kmax, degree, N)
+    P = X.^(-degree) .* Y.^(-degree);
+    
+    vec = linspace(kmin, kmax, kmax-kmin+1);
+    [x,y] = meshgrid(vec, vec);
+    Pnorm = x.^(-degree) .* y.^(-degree);
+    P = P/sum(Pnorm, 'all')*N;
+end
