553bb745aa56e27a7543ec8ee8b47c9ef3ab88c6
Simon Aertssen <Simonaertssen@gmail.com>
2020-12-28 13:53:01 +0100

    gather the time vector on the cpu

--- Functions/DOPRI_simulatenetwork_adaptive.m
+++ Functions/DOPRI_simulatenetwork_adaptive.m
@@ -63,7 +63,7 @@ function [tout, xout, K, Kmeans, p, info] = DOPRI_simulatenetwork_adaptive(ta,tb
             K(:,pulse) = K(:,pulse) + w_i;
             K(pulse,:) = K(pulse,:) + w_o;
             
-            lastspiketimes(pulse) = t;
+            lastspiketimes(pulse) = gather(t);
             dW = gather(window(lastspiketimes - lastspiketimes'));
             % Filter out all zeros that do not contriubute to the learning:
             % that is where lastspiketimes == 0
