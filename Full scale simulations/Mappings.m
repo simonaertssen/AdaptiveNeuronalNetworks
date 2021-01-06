83ca1231de33c19afae63d78cffc70d69d9eca64
Simon Aertssen <Simonaertssen@gmail.com>
2021-01-06 17:12:40 +0100

    made an even better solution for finding zoa!

--- Full scale simulations/Mappings.m
+++ Full scale simulations/Mappings.m
@@ -48,7 +48,7 @@ in(1, m-l:m+l) = 1; in(end, m-l:m+l) = 1; in(m-l:m+l, 1) = 1; in(m-l:m+l, end) =
 %% Theta neurons parameters:
 pars.N = 1000;
 pars.a_n = 0.666666666666666666667;
-seed = 2; rng(seed);
+seed = 3; rng(seed);
 
 %% Draw the problems with mappings, use PSR state:
 labelfont = 17;
@@ -61,7 +61,7 @@ f_mappings = figure('Renderer', 'painters', 'Position', [0,0,800,800]);
 Zstart = -0.2 + 1i*0.8;
 tend = 1.2; col = cm(3,:);
 
-nlines = 20; linalpha = 0.15; cols = zeros(nlines,3);
+nlines = min(30, p.Mk); linalpha = 0.15; cols = zeros(nlines,3);
 idx = randi([1 p.Mk],1,nlines);
 
 % With the simple same IC
@@ -70,14 +70,15 @@ OAIC = ones(p.Mk,1)*Zstart;
 
 % Focus on the ICs:
 subplot(3, 3, 1); hold on; box on; axis square;
-xlim([real(Zstart) - 0.2, real(Zstart) + 0.2]) 
-ylim([imag(Zstart) - 0.2, imag(Zstart) + 0.2]) 
+title('$$z($$\boldmath$$k$$\unboldmath$$, 0) = Z(0)$$', 'Interpreter', 'latex','FontSize', labelfont);
+xlim([real(Zstart) - 0.1, real(Zstart) + 0.1]) 
+ylim([imag(Zstart) - 0.1, imag(Zstart) + 0.1]) 
 drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
 scatter(real(Zstart), imag(Zstart), 500, [0,0,0], 'x', 'LineWidth', linewidth);
-scatter(real(bs(1, idx)), imag(bs(1, idx)), [], cols, 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
+scatter(real(bs(1, idx)), imag(bs(1, idx)), 200, cols, 'x', 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
 bsstart_scatter = scatter(2, 2, [], cols(1,:), 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
 bsstart_scatter.CData(1,:) = bs_plot(1).Color;
 scatter(real(ZOA(1)), imag(ZOA(1)), 500, col, '+', 'LineWidth', linewidth);
@@ -92,12 +93,13 @@ ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfo
 % Show the final state:
 subplot(3, 3, 4); hold on; box on; axis square;
 xlim([real(ZOA(end)) - 0.3, real(ZOA(end)) + 0.1]) 
-ylim([imag(ZOA(end)) - 0.2, imag(ZOA(end)) + 0.2]) 
+ylim([imag(ZOA(end)) - 0.25, imag(ZOA(end)) + 0.15]) 
 drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
 scatter(real(bs(end, idx)), imag(bs(end, idx)), 25, '*k'); 
+scatter(real(bs(end,   :)), imag(bs(end,   :)), 150, '.k', 'MarkerEdgeAlpha', linalpha); 
 plot(real(ZOA(1:end-10)), imag(ZOA(1:end-10)), 'LineWidth', linewidth, 'color', col);
 endline = ZOA(end-3) - ZOA(end);
 endpoint = ZOA(end) + 0.06*endline/abs(endline);
@@ -115,8 +117,9 @@ drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth_total);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
-scatter(real(bs(1, idx)), imag(bs(1, idx)), [], cols, 'LineWidth', linewidth_total, 'MarkerEdgeAlpha', linalpha);
-scatter(real(bs(end, :)), imag(bs(end, :)), 25, '*k'); 
+scatter(real(bs(1, idx)), imag(bs(1, idx)), 100, cols, 'x', 'LineWidth', linewidth_total, 'MarkerEdgeAlpha', linalpha);
+scatter(real(bs(end, idx)), imag(bs(end, idx)), 25, '*k'); 
+scatter(real(bs(end,   :)), imag(bs(end,   :)), 150, '.k', 'MarkerEdgeAlpha', linalpha); 
 scatter(real(Zstart), imag(Zstart), 200, [0,0,0], 'x', 'LineWidth', linewidth_total);
 scatter(real(ZOA(1)), imag(ZOA(1)), 200, col, '+', 'LineWidth', linewidth_total);
 plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth_total, 'color', col);
@@ -128,7 +131,7 @@ plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linew
 phasespaceplot();
 xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
-
+disp("First column")
 
 
 % With the proposed analytical IC
@@ -137,14 +140,15 @@ OAIC = map_Ztozoa(conj(Zstart), p);
 
 % Focus on the ICs:
 subplot(3, 3, 2); hold on; box on; axis square;
-xlim([real(Zstart) - 0.2, real(Zstart) + 0.2]) 
-ylim([imag(Zstart) - 0.2, imag(Zstart) + 0.2]) 
+title('$$z($$\boldmath$$k$$\unboldmath$$, 0) = Z(0) \cdot n($$\boldmath$$k$$\unboldmath$$) / P($$\boldmath$$k$$\unboldmath$$)$$', 'Interpreter', 'latex','FontSize', labelfont);
+xlim([real(Zstart) - 0.1, real(Zstart) + 0.1]) 
+ylim([imag(Zstart) - 0.1, imag(Zstart) + 0.1]) 
 drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
 scatter(real(Zstart), imag(Zstart), 500, [0,0,0], 'x', 'LineWidth', linewidth);
-scatter(real(bs(1, idx)), imag(bs(1, idx)), [], cols, 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
+scatter(real(bs(1, idx)), imag(bs(1, idx)), 200, cols, 'x', 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
 scatter(real(ZOA(1)), imag(ZOA(1)), 500, col, '+', 'LineWidth', linewidth);
 plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
 line([0,real(2*Zstart)], [0, imag(2*Zstart)], 'LineStyle', ':', 'LineWidth', 2, 'Color', 'k');
@@ -156,12 +160,13 @@ ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfo
 % Show the final state:
 subplot(3, 3, 5); hold on; box on; axis square;
 xlim([real(ZOA(end)) - 0.3, real(ZOA(end)) + 0.1]) 
-ylim([imag(ZOA(end)) - 0.2, imag(ZOA(end)) + 0.2]) 
+ylim([imag(ZOA(end)) - 0.25, imag(ZOA(end)) + 0.15]) 
 drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
 scatter(real(bs(end, idx)), imag(bs(end, idx)), 25, '*k'); 
+scatter(real(bs(end,   :)), imag(bs(end,   :)), 150, '.k', 'MarkerEdgeAlpha', linalpha); 
 plot(real(ZOA(1:end-10)), imag(ZOA(1:end-10)), 'LineWidth', linewidth, 'color', col);
 endline = ZOA(end-3) - ZOA(end);
 endpoint = ZOA(end) + 0.06*endline/abs(endline);
@@ -179,8 +184,9 @@ drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth_total);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
-scatter(real(bs(1, idx)), imag(bs(1, idx)), [], cols, 'LineWidth', linewidth_total, 'MarkerEdgeAlpha', linalpha);
-scatter(real(bs(end, :)), imag(bs(end, :)), 25, '*k'); 
+scatter(real(bs(1, idx)), imag(bs(1, idx)), 100, cols, 'x', 'LineWidth', linewidth_total, 'MarkerEdgeAlpha', linalpha);
+scatter(real(bs(end, idx)), imag(bs(end, idx)), 25, '*k'); 
+scatter(real(bs(end,   :)), imag(bs(end,   :)), 150, '.k', 'MarkerEdgeAlpha', linalpha); 
 scatter(real(Zstart), imag(Zstart), 200, [0,0,0], 'x', 'LineWidth', linewidth_total);
 scatter(real(ZOA(1)), imag(ZOA(1)), 200, col, '+', 'LineWidth', linewidth_total);
 plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth_total, 'color', col);
@@ -193,26 +199,28 @@ line([0,real(2*Zstart)], [0, imag(2*Zstart)], 'LineStyle', ':', 'LineWidth', 2, 
 phasespaceplot();
 xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+disp("Second column")
 
 
 
 % With the converged IC
-[OAIC, zoaIC] = findDistribution(Zstart,p);
+[OAIC, zoaIC] = map_Ztozoa_better(Zstart,p,'fmincon');
 [~, ZOA, bs] = OA_simulatenetwork(0, tend, OAIC, p, odeoptions);
 
 % Focus on the ICs:
 subplot(3, 3, 3); hold on; box on; axis square;
-xlim([real(Zstart) - 0.2, real(Zstart) + 0.2]) 
-ylim([imag(Zstart) - 0.2, imag(Zstart) + 0.2]) 
+title('$$\hat{z}($$\boldmath$$k$$\unboldmath$$, 0) \rightarrow z($$\boldmath$$k$$\unboldmath$$, 0)$$', 'Interpreter', 'latex','FontSize', labelfont);
+xlim([real(Zstart) - 0.1, real(Zstart) + 0.1]) 
+ylim([imag(Zstart) - 0.1, imag(Zstart) + 0.1]) 
 drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
-scatter(real(zoaIC), imag(zoaIC), 25, [0,0,0], 'o');
+endidx = randi([1 p.Mk],1,round(p.Mk/3));
+scatter(real(zoaIC(endidx)), imag(zoaIC(endidx)), 25, [0,0,0], 'o');
 zoa_est = scatter(2, 2, 100, [0,0,0], 'o');
-
 scatter(real(Zstart), imag(Zstart), 500, [0,0,0], 'x', 'LineWidth', linewidth);
-scatter(real(bs(1, idx)), imag(bs(1, idx)), [], cols, 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
+scatter(real(bs(1, idx)), imag(bs(1, idx)), 200, cols, 'x', 'LineWidth', linewidth, 'MarkerEdgeAlpha', linalpha);
 scatter(real(ZOA(1)), imag(ZOA(1)), 500, col, '+', 'LineWidth', linewidth);
 plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth, 'color', col);
 
@@ -223,12 +231,13 @@ ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfo
 % Show the final state:
 subplot(3, 3, 6); hold on; box on; axis square;
 xlim([real(ZOA(end)) - 0.3, real(ZOA(end)) + 0.1]) 
-ylim([imag(ZOA(end)) - 0.2, imag(ZOA(end)) + 0.2]) 
+ylim([imag(ZOA(end)) - 0.25, imag(ZOA(end)) + 0.15]) 
 drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
 scatter(real(bs(end, idx)), imag(bs(end, idx)), 25, '*k'); 
+scatter(real(bs(end,   :)), imag(bs(end,   :)), 150, '.k', 'MarkerEdgeAlpha', linalpha); 
 plot(real(ZOA(1:end-10)), imag(ZOA(1:end-10)), 'LineWidth', linewidth, 'color', col);
 endline = ZOA(end-1) - ZOA(end);
 endpoint = ZOA(end) + 0.06*endline/abs(endline);
@@ -246,8 +255,9 @@ drawOAvectors(X + 1i*Y, in, p, cm(2,:));
 
 bs_plot = plot(real(bs(:, idx)), imag(bs(:, idx)), 'LineWidth', linewidth_total);
 for i = 1:nlines; cols(i,:) = bs_plot(i).Color; bs_plot(i).Color(4) = linalpha; end
-scatter(real(bs(1, idx)), imag(bs(1, idx)), [], cols, 'LineWidth', linewidth_total, 'MarkerEdgeAlpha', linalpha);
-scatter(real(bs(end, :)), imag(bs(end, :)), 25, '*k'); 
+scatter(real(bs(1, idx)), imag(bs(1, idx)), 100, cols, 'x', 'LineWidth', linewidth_total, 'MarkerEdgeAlpha', linalpha);
+bsend_scatter = scatter(real(bs(end, idx)), imag(bs(end, idx)), 25, '*k'); 
+scatter(real(bs(end,   :)), imag(bs(end,   :)), 150, '.k', 'MarkerEdgeAlpha', linalpha); 
 scatter(real(Zstart), imag(Zstart), 200, [0,0,0], 'x', 'LineWidth', linewidth_total);
 scatter(real(ZOA(1)), imag(ZOA(1)), 200, col, '+', 'LineWidth', linewidth_total);
 plot(real(ZOA), imag(ZOA), 'LineWidth', linewidth_total, 'color', col);
@@ -259,12 +269,13 @@ plot_arrow(real(endpoint), imag(endpoint), real(ZOA(end)), imag(ZOA(end)),'linew
 phasespaceplot();
 xlabel('Re$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
 ylabel('Im$\left[ \bar{Z}(t)\right]$','Interpreter','latex', 'FontSize', labelfont)
+disp("T column")
 
 pause(0.1);
 l = legend([Zstart_scatter, ZOAstart_scatter, ZOA_plot, zoa_est, bsstart_scatter(1), bs_plot(1), bsend_scatter], ...
-    {'$$Z(0)$$', '$$\bar{Z}(0)$$', '$$\bar{Z}(t)$$', '$$\hat{z}($$\boldmath $$k$$, \unboldmath $$0)$$',...
-    '$$z($$\boldmath $$k$$, \unboldmath $$0)$$', '$$z($$\boldmath $$k$$, \unboldmath $$t)$$', ...
-    '$$z($$\boldmath $$k$$, \unboldmath $$t_{\rm end})$$'}, ...
+    {'$$Z(0)$$', '$$\bar{Z}(0)$$', '$$\bar{Z}(t)$$', '$$\hat{z}($$\boldmath$$k$$\unboldmath$$, 0)$$',...
+    '$$z($$\boldmath$$k$$\unboldmath$$, 0)$$', '$$z($$\boldmath$$k$$\unboldmath$$, t)$$', ...
+    '$$z($$\boldmath$$k$$\unboldmath$$, t_{\rm end})$$'}, ...
     'Location', 'southoutside', 'Orientation', 'horizontal', 'Interpreter', 'latex', 'FontSize', titlefont);
 
 
@@ -275,103 +286,4 @@ set(l,'Position', f1P,'Units', 'normalized');
 set(findall(gcf,'-property','FontName'),'FontName','Avenir')
 
 exportgraphics(f_mappings,'../Figures/PhaseSpace/Mappings.pdf')
-
-
-%% Using fmincon:
-Zstart = -0.2 + 1i*0.9;
-
-[res, IC] = findDistribution(Zstart, p, 'fmincon');
-Z = res*p.P(p.k)/p.N;
-
-close all 
-
-hold on;
-phasespaceplot();
-% xlim([real(Zstart) - 0.2, real(Zstart) + 0.2]) 
-% ylim([imag(Zstart) - 0.2, imag(Zstart) + 0.2]) 
-
-scatter(real(Zstart), imag(Zstart), 100, [0,0,0], 'x', 'LineWidth', 2);
-scatter(real(IC), imag(IC), 20, [0,0,0], 'o');
-scatter(real(res), imag(res), 20, 'x');
-scatter(real(Z), imag(Z), 100, [1,0,0], '+', 'LineWidth', 2);
-
-hold off;
-
-
-%% Functions
-% function res = find(z2D, p)
-%     opts = optimoptions('fmincon','Display','off','FunValCheck','off');
-% 
-%     IC = 
-% end
-
-function [res, IC] = findDistribution(target, p, method)
-    target2D = [real(target); imag(target)];
-    
-    % Define the objective function and its bounds:
-    objfun = @(z2D) norm(z2D*p.P(p.k)/p.N - target2D);
-    onevec = ones(size(target));
-    lb = []; ub = lb;
-    
-    % Define the constraint:
-    function [c,ceq] = cnstrnt(x)
-        c = x(1,:).^2 + x(2,:).^2 - 1;
-%         c = [];
-        ceq = objfun(x);
-    end
-
-    % No other constraints, set those as empty:
-    A = []; b = []; Aeq = []; beq = [];
-
-    % Find distribution of Cartesian ICs within the unit circle
-    Xs = randn(1, p.Mk)*0.01 + real(target);
-    Ys = randn(1, p.Mk)*0.01 + imag(target);
-    magnitude = Xs.^2 + Ys.^2;
-    idx = magnitude > 1;
-    Xs(idx) = Xs(idx) ./ magnitude(idx);
-    Ys(idx) = Ys(idx) ./ magnitude(idx);
-    % Find the ICs 
-    IC = [Xs; Ys];
-    
-    % Solve:
-    if strcmp(method, 'fmincon')
-        nonlcon = @cnstrnt;
-        fminconopts = optimoptions('fmincon','Display','off','Algorithm','sqp');
-        res = fmincon(@(x)0,IC,A,b,Aeq,beq,lb,ub,nonlcon,fminconopts);
-    elseif strcmp(method, 'fsolve')
-        fsolveopts = optimoptions('fsolve','Display','iter','Algorithm','levenberg-marquardt','FunValCheck','off');
-        res = fsolve(objfun,IC,fsolveopts);        
-    end
-   
-    % Return as complex vectors
-    res = res(1,:) + 1i*res(2,:);
-    IC = Xs + 1i*Ys;
-    
-    check = norm(res*p.P(p.k)/p.N - target);
-    disp([string(method), "found solution with accuracy of", num2str(check)])
-end
-
-function [res, IC] = findDistributionOld(target, p)
-    opts = optimoptions('fsolve','Display','off','Algorithm', 'levenberg-marquardt');
-    
-    function s = solveme(rho, theta, target, p)
-        diff = (rho.*exp(1i*wrapToPi(theta)))*p.P(p.k)/p.N - target;
-        s = abs(diff);
-    end
-    
-    Xs = randn(1, p.Mk)*0.05 + real(target);
-    Ys = randn(1, p.Mk)*0.05 + imag(target);
-    magnitude = Xs.^2 + Ys.^2;
-    idx = magnitude > 1;
-    Xs(idx) = Xs(idx) ./ magnitude(idx);
-    Ys(idx) = Ys(idx) ./ magnitude(idx);
-    
-    [TH,R] = cart2pol(Xs,Ys);
-
-    IC = rand(2,p.Mk).*[abs(R); angle(TH)];
-    rhos_thetas = fsolve(@(rhos_thetas) solveme(rhos_thetas(1,:), rhos_thetas(2,:), target, p), IC, opts);
-    res = rhos_thetas(1,:).*exp(1i*wrapToPi(rhos_thetas(2,:)));
-    IC = Xs + 1i*Ys;
-end
-
 
---
+++ Functions/map_Ztozoa_better.m
@@ -0,0 +1,52 @@
+function [zoa, IC] = map_Ztozoa_better(Z0,p,method)
+    if nargin < 3
+        method = 'fmincon';
+    end
+    Z02D = [real(Z0); imag(Z0)];
+    
+    % Define the objective function and its bounds:
+    objfun = @(z2D) norm(z2D*p.P(p.k)/p.N - Z02D);
+    lb = []; ub = lb;
+    
+    % Define the constraint:
+    function [c,ceq] = cnstrnt(x)
+        c = x(1,:).^2 + x(2,:).^2 - 1;
+        ceq = objfun(x);
+    end
+
+    % No other constraints, set those as empty:
+    A = []; b = []; Aeq = []; beq = [];
+
+    % Find distribution of Cartesian ICs within the unit circle
+    Xs = randn(1, p.Mk)*0.02 + real(Z0);
+    Ys = randn(1, p.Mk)*0.02 + imag(Z0);
+    magnitude = Xs.^2 + Ys.^2;
+    idx = magnitude > 1^2;
+    Xs(idx) = Xs(idx) ./ magnitude(idx);
+    Ys(idx) = Ys(idx) ./ magnitude(idx);
+    % Find the ICs 
+    IC = [Xs; Ys];
+    
+    % Solve:
+    if strcmp(method, 'fmincon')
+        nonlcon = @cnstrnt;
+        fminconopts = optimoptions('fmincon','Display','off','Algorithm','active-set','TypicalX',Z02D*ones(1,p.Mk));
+        zoa = fmincon(@(x)0,IC,A,b,Aeq,beq,lb,ub,nonlcon,fminconopts);
+    elseif strcmp(method, 'fsolve')
+        fsolveopts = optimoptions('fsolve','Display','iter','Algorithm','levenberg-marquardt','FunValCheck','off');
+        zoa = fsolve(objfun,IC,fsolveopts);        
+    end
+   
+    % Return as complex vector
+    zoa = zoa(1,:) + 1i*zoa(2,:);
+    IC = Xs + 1i*Ys;
+    
+    check = norm(zoa*p.P(p.k)/p.N - Z0);
+    msg = sprintf("%s found solution with accuracy of %.3e", string(method), check);
+%     if p.Mk > 800 && check > 0.001
+%         error(msg);
+%     else
+%         disp(msg);
+%     end
+    disp(msg);
+end
