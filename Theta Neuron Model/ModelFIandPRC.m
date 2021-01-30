a29300bac9925b0274b212e87c7ef2a1bf0a20bb
Simon Aertssen <Simonaertssen@gmail.com>
2021-01-30 09:35:33 +0100

    Split the figures into fI and PRC

--- Theta Neuron Model/ModelFIandPRC.m
+++ Theta Neuron Model/ModelFIandPRC.m
@@ -12,16 +12,14 @@ set(groot,'DefaultAxesYGrid','on')
 tnow = 0; tend = 10;
 F = @thetaneuron; h = 0.001;
 
-fI = figure('Renderer', 'painters', 'Position', [50 800 1000 200]);
 titlefont = 15;
-labelfont = 20;
-axesfont = 17;
-m = 1; n = 3;
+labelfont = 17;
+axesfont = 12;
+m = 1; n = 2;
 
 %% Investigate the frequency - current curve:
 % We know that T = pi/sqrt(I)
-
-imrow(1) = subplot(m,n,1); hold on; box on;
+fI = figure('Renderer', 'painters', 'Position', [50 800 330 200]); hold on; box on;
 I = @fI_current;
 h = 0.01;
 
@@ -58,9 +56,15 @@ xlabel('$I$','Interpreter','latex', 'FontSize', labelfont)
 ylabel('$\frac{1}{T}$','Interpreter','latex','Rotation', 0, 'FontSize', labelfont*1.5, 'VerticalAlignment','middle', 'HorizontalAlignment','right')
 legend('$\frac{\sqrt{I}}{\pi}$', '$1/\hat{T}$', 'Interpreter','latex', 'FontSize', labelfont, 'Location', 'northwest')
 
+set(findall(gcf,'-property','FontName'),'FontName','Avenir')
+exportgraphics(fI,'../Figures/ThetaNeuronfI.pdf')
+
 %% Draw the explanation about the PRC:
+fPRC = figure('Renderer', 'painters', 'Position', [0 1000 700 200]); hold on; box on;
+
+imrow(1) = subplot(m,n,1); hold on; box on;
 I = 5;
-t = linspace(0, 2, 1001);
+t = 0:0.01:3.2;
 realthetas = @(t) 2*atan(-sqrt(I)*cot(t*sqrt(I)));
 drawrealthetas = spikesNaN(realthetas(t));
 realspiketime = t(isnan(drawrealthetas));
@@ -78,8 +82,6 @@ drawbifthetas(bifidx-1) = NaN;
 
 newvaluetime = fsolve(@(t) realthetas(t) - replacedvalue, phasebifftime, optimoptions('fsolve','Display','off'));
 
-imrow(2) = subplot(m,n,2); hold on; box on;
-hold on
 line([phasebifftime, phasebifftime], [drawrealthetas(bifidx), drawbifthetas(bifidx)], 'LineStyle', '--', 'color', 'k', 'LineWidth', 1)
 
 plot(t, bifthetas, ':k', 'LineWidth', 1, 'HandleVisibility','off');
@@ -88,56 +90,49 @@ plot(t, drawbifthetas, '-r', 'LineWidth', 2);
 plot(t, realthetas(t), ':k', 'LineWidth', 1, 'HandleVisibility','off');
 plot(t, drawrealthetas, '-', 'LineWidth', 2, 'color', '#0072BD');
 
-text(phasebifftime + 0.02, 0.5*(drawbifthetas(bifidx+1) + drawrealthetas(bifidx-1)) + 0.4, '$$\varepsilon$$', 'HorizontalAlignment', 'left', 'FontSize', labelfont, 'Interpreter', 'latex');
+text(phasebifftime + 0.02, 0.5*(drawbifthetas(bifidx+1) + drawrealthetas(bifidx-1)) + 0.5, '$$\varepsilon$$', 'HorizontalAlignment', 'left', 'FontSize', labelfont, 'Interpreter', 'latex');
 
 
 xlabel('Time','Interpreter','tex','FontSize', labelfont, 'FontName', 'SansSerif')
 ylabel('$\theta$','Interpreter','latex', 'FontSize', labelfont)
-set(gca,'XTick',[0, newvaluetime, phasebifftime, realspiketime, bifspiketime], 'XTickLabel',{'0','$$\phi_{\rm{new}}$$','$$\phi$$','$$T$$','$$T_{\phi}$$'}, 'TickLabelInterpreter', 'latex', 'FontSize', axesfont, 'FontName', 'SansSerif')
+set(gca,'XLim', [t(1), t(end)], 'XTick',[0, newvaluetime, phasebifftime, realspiketime(1), bifspiketime(1), realspiketime(2)], 'XTickLabel',{'0','$$\phi_{\rm{new}}$$','$$\phi$$','$$T$$','$$T_{\phi}$$','$$2T$$'}, 'TickLabelInterpreter', 'latex', 'FontSize', axesfont, 'FontName', 'SansSerif')
 set(gca,'YTick',[-pi, -pi/2, replacedvalue, 0, pi/2, pi], 'YTickLabel',{'-$$\pi$$','$$-\frac{\pi}{2}$$',' ','0','$$\frac{\pi}{2}$$','$$\pi$$'}, 'TickLabelInterpreter', 'latex', 'YLim', [-pi-0.2, pi + 0.2], 'FontSize', axesfont)
 
 
 %% Draw the PRC itself:
-imrow(3) = subplot(m,n,3); hold on; box on;
+imrow(2) = subplot(m,n,2); hold on; box on;
 
-I = 2; T = pi/sqrt(I);
-t = linspace(0, T, 100);
-t = t(1:end-1);
+I = 0.1; T = pi/sqrt(I);
+torg = linspace(0, T-0.001, 100);
+t = torg(1:end-1);
 iPRC = 1/(2*I) * (1 - cos(2*pi*t/T));
 realthetas = @(t) 2*atan(-sqrt(I)*cot(t*sqrt(I)));
 realdthetas = @(t) 2*I*csc(t*sqrt(I)).^2./(I*cot(t*sqrt(I)).^2 + 1);
 
-xlim([0, T]);
 yyaxis right;
-plot(t, iPRC, 'LineWidth', 2, 'color', '#D95319')
+es = 0.1:0.1:1;
+for e = es
+    Tf = 1/sqrt(I)*(pi/2 - atan(-e/sqrt(I) + cot(t*sqrt(I)))) - t;
+    PRC = Tf*(2*I);
+    plts = plot(t, PRC, '-', 'LineWidth', 2, 'Color', '#D95319');
+    plts.Color=[0.8500 0.3250 0.0980, e];
+end
+xlim([0, T]);
+% plot(t, iPRC, 'LineWidth', 2, 'color', '#D95319')
 ylabel('\sl PRC', 'FontSize', labelfont, 'FontName', 'SansSerif')
 
 yyaxis left;
-plot(t, realthetas(t), 'LineWidth', 2, 'color', '#0072BD')
-ylabel('$$\theta$$', 'FontSize', labelfont, 'Interpreter', 'latex')
+plot(torg, realthetas(torg), 'LineWidth', 2, 'color', '#0072BD')
+% ylabel('$$\theta$$', 'FontSize', labelfont, 'Interpreter', 'latex')
 set(gca,'YTick',[-pi, -pi/2, 0, pi/2, pi], 'YTickLabel',{'-$$\pi$$','$$-\frac{\pi}{2}$$','0','$$\frac{\pi}{2}$$','$$\pi$$'}, 'TickLabelInterpreter', 'latex', 'YLim', [-pi-0.2, pi + 0.2], 'FontSize', axesfont)
 ax = gca; ax.YAxis(1).Color = 'k'; ax.YAxis(2).Color = '#D95319';
-
-
-% es = logspace(-2, -1, 5)
-% es = linspace(0.001, 0.05, 5);
-% phi = t;
-% 
-% for e = es
-%     Tf = 1/sqrt(I)*(pi/2 - atan(e/sqrt(I) + cot(phi*sqrt(I)))) - phi;
-%         PRC = - Tf*(2*I);
-%     plot(phi, PRC, 'LineWidth', 2)
-% end
-
 xlabel('$\phi$','Interpreter','latex', 'FontSize', labelfont)
-
 set(gca,'XTick',[0, T/2, T], 'XTickLabel',{'0','$$T/2$$','$$T$$'}, 'TickLabelInterpreter', 'latex')
 
 
 %%
 set(findall(gcf,'-property','FontName'),'FontName','Avenir')
-exportgraphics(fI,'../Figures/ThetaNeuronfIandPRC.pdf')
-
+exportgraphics(fPRC,'../Figures/ThetaNeuronPRC.pdf')
 
 %% Functions:
 
